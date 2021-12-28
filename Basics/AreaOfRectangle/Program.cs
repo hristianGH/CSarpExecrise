df699f53ba73bcf80a4403a42d5040f2506eee03
hristianGH <hristovhristian7@gmail.com>
2021-12-28 20:20:10 +0200

    Trying stuff

---
+++ Basics/AreaOfRectangle/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AreaOfRectangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = double.Parse(Console.ReadLine());
+            var b = double.Parse(Console.ReadLine());
+            Console.WriteLine($"Area size is {a*b}");
+        }
+    }
+}
---
+++ Basics/InchToCm/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace InchToCm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter Inches");
+                double input = double.Parse(Console.ReadLine());
+                Console.WriteLine($"{input*2.54} cm");
+
+            }
+        }
+    }
+}
