35f78577a2ba3c9bd6111afc2c53452c2f9d1482
hristianGH <hristovhristian7@gmail.com>
2021-12-29 01:10:52 +0200

    UsdToBgn Converter

---
+++ Basics/USDtoBGN/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace USDtoBGN
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            double value = 0;
+            string endCurrency = "";
+            Console.WriteLine("Press y for USD");
+            Console.WriteLine("Press n for BGN");
+            if (Console.ReadKey().KeyChar == 'y')
+            {
+                Console.WriteLine();
+                Console.WriteLine("Enter USD");
+                value = double.Parse(Console.ReadLine());
+                value = value * 1.79549;
+                endCurrency = "BGN";
+            }
+            else
+            {
+                Console.WriteLine();
+                Console.WriteLine("Enter BGN");
+                value = double.Parse(Console.ReadLine());
+                value = value / 1.79549;
+                endCurrency = "USD";
+            }
+            Console.WriteLine(value);
+        }
+    }
+}
+//1 USD = 1.79549 BGN
