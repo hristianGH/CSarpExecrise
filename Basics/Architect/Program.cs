0f21b2dc89181cc106a99b05810e1bea5f28bdcb
hristianGH <hristovhristian7@gmail.com>
2021-12-28 23:06:16 +0200

    RectangleArea and Architect

---
+++ Basics/Architect/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Architect
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "";
+            int projectCount =0;
+            int timeNeeded=3;
+            Console.WriteLine("Enter architect name");
+            name = Console.ReadLine();
+            Console.WriteLine("Enter project count");
+            projectCount = int.Parse(Console.ReadLine());
+            Console.WriteLine($"{name} will need {projectCount*timeNeeded} to complete {projectCount} projects");
+        }
+    }
+}
---
+++ Basics/PersonInfoFromConsole/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace PersonInfoFromConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first name");
+            var fName = Console.ReadLine();
+            Console.WriteLine("Enter Lirst name");
+            var lName = Console.ReadLine();
+            Console.WriteLine("Enter Age");
+            int age = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Town");
+            var town = Console.ReadLine();
+            Console.WriteLine($"{fName} {lName} from {town} is {age} years old");
+
+        }
+    }
+}
