93a980786ab7f57811e35f68a02db5832faf21f5
hristianGH <hristovhristian7@gmail.com>
2021-12-28 23:20:42 +0200

    GardenerAndZooShop

---
+++ Basics/Gardener/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Gardener
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double sqrGardened = double.Parse(Console.ReadLine());
+            double dds = 7.61;
+            double discount = 0.18;
+            sqrGardened=(sqrGardened*dds)-(sqrGardened*discount);
+        }
+    }
+}
---
+++ Basics/ZooShopExpences/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ZooShopExpences
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double sum = 0;
+            string command = Console.ReadLine();
+            while (command.ToLower()!="end")
+            {
+                sum+=double.Parse(Console.ReadLine());
+                command = Console.ReadLine();
+            }
+            Console.WriteLine($"{sum:f2}");
+        }
+    }
+}
