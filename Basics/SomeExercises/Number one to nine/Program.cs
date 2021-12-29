df3568ae50c514d814637380a738cf218d303291
hristianGH <hristovhristian7@gmail.com>
2021-12-29 11:10:02 +0200

    Some Exercise

---
+++ Basics/SomeExercises/Number one to nine/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _04._Number_100_200
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            if (num == 1)
+            {
+                Console.WriteLine("one");
+            }
+            else  if (num == 2)
+            {
+                Console.WriteLine("two");
+            }
+            else if (num == 3)
+            {
+                Console.WriteLine("three");
+            }
+            else if (num == 4)
+            {
+                Console.WriteLine("four");
+            }
+        }
+    }
+    }
+
