0462e0649e5f81c6ff0bbac93a8c35bdf62c3281
hristianGH <hristovhristian7@gmail.com>
2021-12-29 10:36:54 +0200

    Fixing Folder and some exercises

---
+++ Basics/SomeExercises/Password Guess/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _05._Password_Guess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pass1 = Console.ReadLine();
+           // string pass = "s3cr3t!P @ssw0rd";
+            if (pass1 == "s3cr3t!P @ssw0rd") 
+            {
+                Console.WriteLine("Welcome");
+            }
+            else
+            {
+                Console.WriteLine("Wrong password");
+            }
+        }
+    }
+}
---
+++ Basics/SomeExercises/Toy Shop/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace _07._Toy_Shop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double puzzle = 2.60;
+            double talkingDoll = 3.00;
+            double stuffedTeddy = 4.10;
+            double toyMinion = 8.20;
+            double toyTruck = 2.00;
+            
+            //
+            double vacantionPrice = double.Parse(Console.ReadLine());
+            int numPuzzle = int.Parse(Console.ReadLine());
+            int numTalkingDoll = int.Parse(Console.ReadLine());
+            int numStuffedTeddy = int.Parse(Console.ReadLine());
+            int numToyMinion = int.Parse(Console.ReadLine());
+            int numToyTruck = int.Parse(Console.ReadLine());
+            //
+            //
+            double sumPuzzle = puzzle * numPuzzle;
+            double sumTalkingDoll = talkingDoll * numTalkingDoll;
+            double sumStuffedTeddy = stuffedTeddy * numStuffedTeddy;
+            double sumToyMinion = toyMinion * numToyMinion;
+            double sumToyTruck = toyTruck * numToyTruck;
+            //
+            double priceAllToys = sumPuzzle + sumTalkingDoll + sumStuffedTeddy + sumToyMinion + sumToyTruck;
+            //
+            //
+            int numOfToys = numPuzzle + numStuffedTeddy + numTalkingDoll + numToyMinion + numToyTruck;
+            
+           
+            //
+         
+        if (numOfToys >= 50 )
+       {
+           priceAllToys *= 0.75;
+       }
+         
+        double Rent = (priceAllToys * 0.10);
+         
+           double winnings = priceAllToys - Rent;
+          
+         
+          if (winnings >=vacantionPrice)
+          {
+              Console.WriteLine($"Yes! {winnings - vacantionPrice:F2} lv left.");
+          }
+          else
+              Console.WriteLine($"Not enough money! {vacantionPrice - winnings:F2} lv needed.");
+        }
+    }
+}
