8f810934a10dcafdea2bb0b5b041d62a8f1a6eb4
hristianGH <hristovhristian7@gmail.com>
2021-12-30 21:26:12 +0200

    Time,Godzila vs Kong

---
+++ Basics/SomeExercises/Two/05. Time + 15 Minutes/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace _05._Time___15_Minutes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int hour = int.Parse(Console.ReadLine());
+            int min = int.Parse(Console.ReadLine());
+
+               int timeinmin = hour * 60 + min;
+            int timeinmin15 = timeinmin + 15;
+            int hourAfter15 = timeinmin15 / 60;
+            int minAfter15 = timeinmin15 % 60;
+            if (hourAfter15==24)
+            {
+                hourAfter15 = 0;
+            }
+            if (minAfter15==60)
+            {
+                minAfter15 = 0;
+                hourAfter15 =+1;
+
+            }
+            Console.WriteLine($"{hourAfter15}:{minAfter15:d2}");
+
+            
+
+        }
+    }
+}
---
+++ Basics/SomeExercises/Two/06. Godzilla vs. Kong/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _06._Godzilla_vs._Kong
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            int statist = int.Parse(Console.ReadLine());
+            double priceCostumesPerStatist = double.Parse(Console.ReadLine());
+            double costumesPrice = priceCostumesPerStatist * statist;
+            double decoration = budget * 0.10;
+            if (statist>=150)
+            {
+                costumesPrice = costumesPrice * 0.9;
+            }
+            double decoreAndCostumes = decoration + costumesPrice;
+            if (decoreAndCostumes > budget)
+            {
+                Console.WriteLine("Not enough money!");
+                Console.WriteLine($"Wingard needs {decoreAndCostumes - budget:f2} leva more.");
+
+               
+            }
+            else 
+            {
+                Console.WriteLine("Action!");
+                Console.WriteLine($"Wingard starts filming with {budget - decoreAndCostumes:f2} leva left.");
+            }
+        }
+    }
+}
