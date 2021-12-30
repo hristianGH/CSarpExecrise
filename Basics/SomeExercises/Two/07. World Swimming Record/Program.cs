27d0343a63b7116cbdb4b8c4846882992e4264c6
hristianGH <hristovhristian7@gmail.com>
2021-12-30 21:32:10 +0200

    Swimming record

---
+++ Basics/SomeExercises/Two/07. World Swimming Record/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _07._World_Swimming_Record
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double recordInSec = double.Parse(Console.ReadLine());
+            double distanceMetre = double.Parse(Console.ReadLine());
+            double timeInSec = double.Parse(Console.ReadLine());
+            double swimDistance = distanceMetre * timeInSec;
+            double waterSlowDown = Math.Floor(distanceMetre / 15) * 12.5;
+           
+            
+          // double waterSlowDown2= distanceMetre * 12.5;
+            double sumTime = swimDistance + waterSlowDown;
+
+
+            
+       
+       if(recordInSec>sumTime)
+       {
+           Console.WriteLine($"Yes, he succeeded! The new world record is {sumTime:f2} seconds.");
+       }
+       else
+       {
+           Console.WriteLine($"No, he failed! He was {sumTime-recordInSec:f2} seconds slower.");
+       }
+       
+       }
+    } 
+}
+
