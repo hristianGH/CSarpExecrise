4f679f1ed36f0f3d689182429536ad467e821d56
hristianGH <hristovhristian7@gmail.com>
2021-12-31 22:23:37 +0200

    FInalThree

---
+++ Basics/SomeExercises/Three/11. Fruit Shop 2/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Threading;
+
+namespace _11._Fruit_Shop_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fruit = Console.ReadLine();
+            string date = Console.ReadLine();
+            double num = double.Parse(Console.ReadLine());
+            double sum = 0.00;
+            //
+            switch(date)
+            {
+                case "Monday":
+                case "Tuesday":
+                case "Wednesday":
+                case "Thursday":
+                case "Friday":
+                    switch(fruit)
+                    {
+                        case ("banana"):
+                            sum = 2.50 * num;
+                            break;
+                        case ("apple"):
+                            sum = 1.20 * num;
+                            break;
+                        case ("orange"):
+                            sum = 0.85 * num;
+                            break;
+                        case ("grapefruit"):
+                            sum = 1.45 * num;
+                            break;
+                        case ("kiwi"):
+                            sum = 2.70 * num;
+                            break;
+                        case ("pineapple"):
+                            sum = 5.50 * num;
+                            break;
+                        case ("grapes"):
+                            sum = 3.85 * num;
+                            break;
+                        default:
+                            Console.WriteLine("error");
+                            break;
+                    }
+                    break;
+                   case "Saturday":
+                    case "Sunday":
+                    switch(fruit)
+                    {
+                        case ("banana"):
+                            sum = 2.70 * num;
+                            break;
+                        case ("apple"):
+                            sum = 1.25 * num;
+                            break;
+                        case ("orange"):
+                            sum = 0.90 * num;
+                            break;
+                        case ("grapefruit"):
+                            sum = 1.60 * num;
+                            break;
+                        case ("kiwi"):
+                            sum = 3.00 * num;
+                            break;
+                        case ("pineapple"):
+                            sum = 5.60 * num;
+                            break;
+                        case ("grapes"):
+                            sum = 4.20 * num;
+                            break;
+                        default:
+                            Console.WriteLine("error");
+                            break;
+                    }
+                    break;
+                           default:
+                    {
+                        Console.WriteLine("error");
+                    }
+                    break;
+                  
+                    
+            }
+
+            if (sum > 0)
+            {
+                Console.WriteLine();
+
+
+
+                Console.WriteLine($"{sum:f2}");
+            }
+        }
+
+
+    }
+}
---
+++ Basics/SomeExercises/Three/11. Fruit Shop/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace _11._Fruit_Shop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fruit = Console.ReadLine();
+            string day = Console.ReadLine();
+            double num = double.Parse(Console.ReadLine());
+            double price = 0.0;
+            switch (day)
+            {
+
+                case ("Monday"):
+                case ("Tuesday"):
+                case ("Wednesday"):
+                case ("Thursday"):
+                case ("Friday"):
+                    {
+                        switch (fruit)
+                        {
+                            case ("banana"):
+                                price = 2.50 * num;
+                                Console.WriteLine(price);
+                                break;
+                            case ("apple"):
+                                price = 1.20 * num;
+                                Console.WriteLine(price);
+                                break;
+                            case ("orange"):
+                                price = 0.85 * num;
+                                Console.WriteLine(price);
+                                break;
+                            case ("grapefruit"):
+                                price = 1.45 * num;
+                                Console.WriteLine(price);
+                                break;
+                            case ("kiwi"):
+                                price = 2.70 * num;
+                                Console.WriteLine(price);
+                                break;
+                            case ("pineapple"):
+                                price =5.50 * num;
+                                Console.WriteLine(price);
+                                break;
+                            case ("grapes"):
+                                price = 3.85 * num;
+                                Console.WriteLine(price);
+                                break;
+                            default:
+                                Console.WriteLine("error");
+                                break;
+                        }
+                    }
+                    break;
+                case "Saturday":
+                case "Sunday":
+                    {
+
+                    }
+                    {
+                        switch (fruit)
+                        {
+                            case ("banana"):
+                                price = 2.70 * num;
+                                Console.WriteLine(price);
+                                break;
+                            case ("apple"):
+                                price = 1.25 * num;
+                                Console.WriteLine(price);
+                                break;
+                            case ("orange"):
+                                price = 0.90 * num;
+                                Console.WriteLine(price);
+                                break;
+                            case ("grapefruit"):
+                                price = 3.00 * num;
+                                Console.WriteLine(price);
+                                break;
+                            case ("kiwi"):
+                                price = 5.60 * num;
+                                Console.WriteLine(price);
+                                break;
+                            case ("pineapple"):
+                                price = 5.60 * num;
+                                Console.WriteLine( price: f2);
+                                break;
+                            case ("grapes"):
+                                price = 4.20 * num;
+                                Console.WriteLine($"{price:f2}");
+                                break;
+
+                            default:
+                                Console.WriteLine("error");
+                                break;
+                        }
+                        break;
+
+                    }
+                    
+                   
+                default:
+                    Console.WriteLine("error");
+                    break;
+
+
+            }
+
+
+
+
+
+        }
+    }
+}
