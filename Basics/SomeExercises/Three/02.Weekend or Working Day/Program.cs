bc159be39204fdd2ef647a9b04a1ebaee0940257
hristianGH <hristovhristian7@gmail.com>
2021-12-31 19:39:26 +0200

    1,2,3 exercise

---
+++ Basics/SomeExercises/Three/02.Weekend or Working Day/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace _02.Weekend_or_Working_Day
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+
+   
+            {
+                string day = Console.ReadLine();
+                switch (day)
+                {
+                    case "Monday":
+                                                                                      // Console.WriteLine("Monday");
+                                                                                     // break;
+                    case "Tuesday":
+                                                                           // Console.WriteLine("Tuesday");
+                                                                             // break;
+                    case "Wednesday":
+                                                                       //  Console.WriteLine("Wednesday");
+                                                                      // break;
+                    case "Thursday":
+                                                                       // Console.WriteLine("Thursday");
+                                                                            // break;
+                    case "Friday":
+                                                                    // Console.WriteLine("Friday");
+                        Console.WriteLine("Working day");
+                        break;
+                    case "Saturday":
+                                                                 //Console.WriteLine("Saturday");
+                                                              // break;
+                    case "Sunday":
+                                                                 // Console.WriteLine("Sunday");
+                        Console.WriteLine("Weekend");
+                        break;
+                    default:
+                        Console.WriteLine("Error");
+                        break;
+                }
+            }
+        }
+    }
+
+}
+    
+
