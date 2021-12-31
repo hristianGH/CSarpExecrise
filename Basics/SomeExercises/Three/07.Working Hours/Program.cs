7c03c97412cd91d0def2f8f6a03bda6196b53fbe
hristianGH <hristovhristian7@gmail.com>
2021-12-31 22:21:19 +0200

    SomeExercise

---
+++ Basics/SomeExercises/Three/07.Working Hours/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _07.Working_Hours
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hour = int.Parse(Console.ReadLine());
+            string day = Console.ReadLine();
+            switch (day)
+            {
+                case "Monday":
+                case "Tuesday":
+                case "Wednesday":
+                case "Thursday":
+                case "Friday":
+                case "Saturday":
+
+                    if (hour <= 18 && hour >= 10)
+                    {
+                        Console.WriteLine("Open");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Closed");
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Closed");
+                    break;
+            }
+        }
+    }
+}
