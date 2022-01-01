67539c3a5aaa52c33ae0d15dfa09738384abb8be
hristianGH <hristovhristian7@gmail.com>
2022-01-01 23:30:22 +0200

    SomeExercise

---
+++ Basics/SomeExercises/Four/03. New House/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Threading;
+
+namespace _03._New_House
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double rosePrice = 5.00;
+            double dahliasPrice = 3.80;
+            double tylipsPrice = 2.80;
+            double narcissusPrice = 3.00;
+            double gladiolusPrice = 2.50;
+            //
+            string typeFlower = Console.ReadLine();
+            int numFlower = int.Parse(Console.ReadLine());
+            int budghet = int.Parse(Console.ReadLine());
+            double flowerSum = 0.00;
+            //
+
+            if (typeFlower == "Roses")
+            {
+                flowerSum = rosePrice * numFlower;
+                if (numFlower >= 80)
+                {
+                    flowerSum *= 0.90;
+                }
+
+            }
+
+            else if (typeFlower == "Dahlias")
+            {
+                flowerSum = dahliasPrice * numFlower;
+                if (numFlower >= 90)
+                {
+                    flowerSum *= 0.85;
+
+                }
+            }
+
+            else if (typeFlower == "Tulips")
+            {
+                flowerSum = tylipsPrice * numFlower;
+                if (numFlower >= 80)
+                {
+                    flowerSum *= 0.85;
+                }
+            }
+
+            else if (typeFlower == "Narcissus")
+            {
+                flowerSum = narcissusPrice * numFlower;
+                if (numFlower <= 120)
+                {
+                    flowerSum *= 1.15;
+                }
+            }
+
+            else if (typeFlower == "Gladiolus")
+            {
+                flowerSum = gladiolusPrice * numFlower;
+                if (numFlower <= 80)
+                {
+                    flowerSum *= 1.20;
+                }
+            }
+                if(flowerSum<budghet)
+            {
+                Console.WriteLine($"Hey, you have a great garden with {numFlower} {typeFlower} and {budghet-flowerSum:f2} leva left.");
+            }
+            else
+            {
+                Console.WriteLine($"Not enough money, you need {flowerSum-budghet:f2} leva more.");
+            }
+        }
+    }
+}
---
+++ Basics/SomeExercises/Four/04. Fishing Boat/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Threading;
+
+namespace _04._Fishing_Boat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int groupBudget = int.Parse(Console.ReadLine());
+            string season = Console.ReadLine();
+            int numFishers = int.Parse(Console.ReadLine());
+            double sum = 0.00;
+            //
+            if (season == "Spring")
+            {
+                sum = sum + 3000;
+            }
+            else if (season == "Summer" || season == "Autumn")
+            {
+                sum = sum + 4200;
+            }
+            else if (season == "Winter")
+            {
+                sum = sum + 2600;
+            }
+
+            if (numFishers <= 6)
+            {
+                sum = sum * 0.90;
+            }
+            else if (numFishers >=7 && numFishers<=11)
+            {
+                sum = sum * 0.85;
+            }
+            else if (numFishers >12  )
+            {
+                sum = sum * 0.75;
+            }
+            
+             if (numFishers % 2 == 0 && season != "Autumn")
+            {
+                sum = sum *0.95;
+            }
+
+             if (groupBudget>=sum)
+            {
+                Console.WriteLine($"Yes! You have {groupBudget-sum:f2} leva left.");
+            }
+             else
+            {
+                Console.WriteLine($"Not enough money! You need {sum-groupBudget:f2} leva.");
+            }
+        }
+    }
+}
