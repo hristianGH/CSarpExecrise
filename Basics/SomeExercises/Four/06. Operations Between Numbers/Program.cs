511e161a62e93ec26c30b19d9f32e32f38f6e02e
hristianGH <hristovhristian7@gmail.com>
2022-01-01 23:30:51 +0200

    More

---
+++ Basics/SomeExercises/Four/06. Operations Between Numbers/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Threading;
+
+namespace _06._Operations_Between_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            double result = 0.00;
+            string sym = Console.ReadLine();
+            string evenOrodd = "";
+              
+            //
+            switch (sym)
+            {
+                case "+":
+                    result = (num1 + num2);
+                    if (result % 2 == 0)
+                    {
+                        evenOrodd = "even";
+                    }
+                    else
+                    {
+                        evenOrodd = "odd";
+                    }
+                    break;
+
+                     case "-":
+                    result = (num1 - num2);
+                    if (result % 2 == 0)
+                        {
+                        evenOrodd = "even";
+                    }
+                    else
+                    {
+                        evenOrodd = "odd";
+                    }
+                    break;
+
+                     case "*":
+                    result = (num1 * num2);
+                    if (result % 2 == 0)
+                    {
+                        evenOrodd = "even";
+                    }
+                    else
+                    {
+                        evenOrodd = "odd";
+                    }
+
+                    break;
+             
+                   case "/":
+                  result = num1 / num2;
+                  if (num2 != 0)
+                  { Console.WriteLine($"{num1} / {num2} = {num1 / num2:F2}"); 
+                  }
+                  else
+                  {
+                      Console.WriteLine($"Cannot divide {num1} by zero");
+                  }
+                  Math.Round(result, 2);
+                  break;
+              case "%":
+                  result = num1 % num2;
+                  break;
+            }
+
+                 Console.WriteLine(result);
+        }
+    }
+}
