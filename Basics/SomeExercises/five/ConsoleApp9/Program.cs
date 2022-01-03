e9aa5488fd2d10413795ba8b59946ed5aad76625
hristianGH <hristovhristian7@gmail.com>
2022-01-03 21:24:32 +0200

    exercise

---
+++ Basics/SomeExercises/five/ConsoleApp9/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string type = Console.ReadLine();
+            int rows = int.Parse(Console.ReadLine());
+            int colums = int.Parse(Console.ReadLine());
+            double money = 0.0;
+             if (type=="Premiere")
+            {
+                money = 12.00;
+            }
+             else if (type=="Normal")
+            {
+                money = 7.50;
+            }
+             else if (type=="Discount")
+            {
+                money = 5.00;
+
+            }
+            double calculations = (rows * colums) * money;
+            Console.WriteLine($"{calculations:f2} leva");
+        }
+    }
+}
