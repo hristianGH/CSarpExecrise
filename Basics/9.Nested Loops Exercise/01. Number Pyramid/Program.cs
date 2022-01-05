850078641ddcadad687ff99d3172b7817e99f919
hristianGH <hristovhristian7@gmail.com>
2022-01-05 15:57:46 +0200

    Nested Loop Exercise

---
+++ Basics/9.Nested Loops Exercise/01. Number Pyramid/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _01._Number_Pyramid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int num = 1;
+            bool breaker = false;
+            for (int numH = 0; numH <= n; numH++)
+            {
+                for (int numW = 0; numW <= numH; numW++)
+                {
+                    if (num > n)
+                    {
+                        breaker = true;
+                        break;
+                    }
+                    Console.Write(num +" ");
+                    num++;
+                }
+                if (breaker==true)
+                {
+                    break;
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Basics/9.Nested Loops Exercise/num piracit/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace num_piracit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int imput = int.Parse(Console.ReadLine());
+            int num = 1;
+            int height = 0;
+            int width = 0;
+            while (height<imput)
+            {
+                height++;
+                while (width<height)
+                {
+                    Console.Write(num + " ");
+                    num++;
+                    width++;
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
