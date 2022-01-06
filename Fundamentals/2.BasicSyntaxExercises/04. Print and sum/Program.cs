60e27442987a7339c8874797b794271ccc113922
hristianGH <hristovhristian7@gmail.com>
2022-01-06 18:52:20 +0200

    4.PrintSum

---
+++ Fundamentals/2.BasicSyntaxExercises/04. Print and sum/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _04._Print_and_sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputOne = int.Parse(Console.ReadLine());
+            int inputTwo = int.Parse(Console.ReadLine());
+            int sum = 0;
+            for (int i = inputOne; i <=inputTwo; i++)
+            {
+                sum += i;
+                Console.Write($"{i} ");
+            }
+                Console.WriteLine();
+                Console.WriteLine($"Sum: {sum}");
+        }
+    }
+}
