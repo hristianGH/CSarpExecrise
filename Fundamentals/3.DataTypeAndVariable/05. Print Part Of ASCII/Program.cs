1997e7aeb0d9ba6821f4881523a365c4acbd92bf
hristianGH <hristovhristian7@gmail.com>
2022-01-07 23:28:43 +0200

    5.PrintAscii

---
+++ Fundamentals/3.DataTypeAndVariable/05. Print Part Of ASCII/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _05._Print_Part_Of_ASCII
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int begining = int.Parse(Console.ReadLine());
+            int end = int.Parse(Console.ReadLine());
+
+            for (int i = begining; i <= end; i++)
+            {
+            char letter =(char) i;
+
+                Console.Write(letter + " ");
+
+            }
+        }  
+    }
+}
---
+++ Fundamentals/3.DataTypeAndVariable/3. Elevator the actual/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _3._Elevator_the_actual
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int people = int.Parse(Console.ReadLine());
+            int space = int.Parse(Console.ReadLine());
+            int courses = 0;
+            while (people>0)
+            {
+                people = people - space;
+                courses++;
+            }
+            Console.WriteLine(courses);
+        }
+    }
+}
