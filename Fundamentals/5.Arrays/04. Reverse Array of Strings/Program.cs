3f9bef7fad9dcbbc1a8b7736f8f74e7f5ad934ab
hristianGH <hristovhristian7@gmail.com>
2022-01-08 21:14:25 +0200

    4.ReverseArray

---
+++ Fundamentals/5.Arrays/04. Reverse Array of Strings/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _04._Reverse_Array_of_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             string imput = Console.ReadLine();
+            string[] imputSepperator = imput.Split(' ');
+            int imputLenght = imputSepperator.Length;
+            for (int i =imputLenght - 1 ; i >= 0; i--)
+            {
+                Console.Write($"{imputSepperator[i]} ");
+            }
+        }
+    }
+}
