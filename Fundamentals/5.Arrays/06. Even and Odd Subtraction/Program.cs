5400966602a6f95bac79690aefd0687bf66e0bcd
hristianGH <hristovhristian7@gmail.com>
2022-01-08 21:43:55 +0200

    6.Subtraction

---
+++ Fundamentals/5.Arrays/06. Even and Odd Subtraction/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _06._Even_and_Odd_Subtraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string imput = Console.ReadLine();
+            string[] collage = imput.Split(' ');
+            int evenNum = 0;
+            int oddNum = 0;
+            for (int i = 0; i < collage.Length; i++)
+            {
+                int converter = int.Parse(collage[i]);
+                if (converter % 2 == 0)
+                {
+                    evenNum = evenNum + converter;
+                }
+                else
+                {
+                    oddNum = oddNum + converter;
+                }
+
+            }
+            evenNum = evenNum - oddNum;
+            Console.WriteLine(evenNum);
+        }
+    }
+}
