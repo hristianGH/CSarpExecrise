2f16d899681fa163ad2d959f01b0bbc66c966f62
hristianGH <hristovhristian7@gmail.com>
2022-01-08 21:40:14 +0200

    5.SumEven

---
+++ Fundamentals/5.Arrays/05. Sum Even Numbers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _05._Sum_Even_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string imput = Console.ReadLine();
+            string[] collage = imput.Split(' ');
+            int evenNum = 0;
+            for (int i = 0; i < collage.Length; i++)
+            {
+                int converter = int.Parse(collage[i]);
+                if (converter%2==0)
+                {
+                    evenNum = evenNum + converter;
+                }
+                else 
+                {
+
+                }
+                 
+            }
+            Console.WriteLine(evenNum);
+        }
+    }
+}
