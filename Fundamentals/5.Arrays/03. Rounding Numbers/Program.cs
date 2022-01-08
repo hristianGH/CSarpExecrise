f4490919647a2d0a37d80e3240e42a37e34e6e0a
hristianGH <hristovhristian7@gmail.com>
2022-01-08 17:27:59 +0200

    3,RoundingNumbers

---
+++ Fundamentals/5.Arrays/03. Rounding Numbers/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _03._Rounding_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string imput = Console.ReadLine();
+            string[] imputArray = imput.Split(' ');
+
+            for (int i = 0; i < imputArray.Length; i++)
+            {
+                double arrayToNum = double.Parse(imputArray[i]);
+                Math.Round(arrayToNum);
+                 
+                Console.WriteLine($"{imputArray[i]} => {Math.Round(arrayToNum,MidpointRounding.AwayFromZero)} ");
+
+                //Console.WriteLine($"{arrayToNum:f0}");
+
+
+            }
+
+
+        }
+    }
+}
