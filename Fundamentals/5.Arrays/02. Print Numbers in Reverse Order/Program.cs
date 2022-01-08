63d41f99276dee49026f3517de8c20dc7145738d
hristianGH <hristovhristian7@gmail.com>
2022-01-08 17:03:31 +0200

    2.printreverse

---
+++ Fundamentals/5.Arrays/02. Print Numbers in Reverse Order/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _02._Print_Numbers_in_Reverse_Order
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] numberPocket = new int[n];
+            for (int i = 0; i <n ; i++)
+            {
+                numberPocket[i] = int.Parse(Console.ReadLine());
+            }
+            for (int y = n; y >0 ; y--)
+            {
+                Console.Write ($"{numberPocket[y-1]} ");
+            }
+        }
+    }
+}
