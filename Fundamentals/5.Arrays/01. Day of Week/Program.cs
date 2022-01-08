e7e118ebdffb644688441586e381427511019281
hristianGH <hristovhristian7@gmail.com>
2022-01-08 16:38:30 +0200

    1.DayOfWeek

---
+++ Fundamentals/5.Arrays/01. Day of Week/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _01._Day_of_Week
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] daysOfWeek = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday", };
+            int day = int.Parse(Console.ReadLine());
+            if (day > 0 && day < 8)
+            {
+                day--;
+                Console.WriteLine(daysOfWeek[day]);
+            }
+            else
+            {
+                Console.WriteLine("Invalid day!");
+            }
+
+        }
+    }
+}
+
