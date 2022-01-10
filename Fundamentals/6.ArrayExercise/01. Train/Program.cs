7fedab61e7539723c2e6033b9606e4a7ec3c9d1d
hristianGH <hristovhristian7@gmail.com>
2022-01-10 15:59:07 +0200

    ArrayExercises

---
+++ Fundamentals/6.ArrayExercise/01. Train/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _01._Train
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] arrWagons = new int[n];
+            int sum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                arrWagons[i] = int.Parse(Console.ReadLine());
+                sum = arrWagons[i] + sum;
+            }
+            for (int i = 0; i < arrWagons.Length; i++)
+            {
+            Console.Write($"{arrWagons[i]} ");
+
+
+            }
+            Console.WriteLine(Environment.NewLine + sum);
+        }
+    }
+}
---
+++ Fundamentals/6.ArrayExercise/02. Common Elements/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _02._Common_Elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string imputFirst = Console.ReadLine();
+            string[] arrayFirst = imputFirst.Split(); 
+            string imputSecond = Console.ReadLine();
+            string[] arraySecond = imputSecond.Split();
+            for (int i = 0; i < arraySecond.Length; i++)
+            {
+                for (int y = 0; y < arrayFirst.Length; y++)
+                {
+
+                    if (arraySecond[i]==arrayFirst[y])
+                    {
+                        Console.Write($"{arraySecond[i]} ");
+                    }
+
+                }
+            }
+
+        }
+    }
+}
