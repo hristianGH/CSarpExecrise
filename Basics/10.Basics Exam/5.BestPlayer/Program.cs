fa0702a06cdd8ba69d8fb6d69c53b181f4e06514
hristianGH <hristovhristian7@gmail.com>
2022-01-10 19:17:12 +0200

    BestPlayerExam

---
+++ Basics/10.Basics Exam/5.BestPlayer/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _5.BestPlayer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string bestPlayer = "";
+            int mostGoals = 0;
+            string playerName = Console.ReadLine();
+            while (playerName.ToLower() != "end")
+            {
+                int countGoals = int.Parse(Console.ReadLine());
+                if (countGoals > mostGoals)
+                {
+                    bestPlayer = playerName;
+                    mostGoals = countGoals;
+                }
+                playerName = Console.ReadLine();
+
+            }
+            if (mostGoals>=3)
+            {
+                Console.WriteLine($"{bestPlayer} is the best player!{Environment.NewLine}He has scored {mostGoals} goals and made a hat-trick !!!");
+
+            }
+            else
+            {
+                Console.WriteLine($"{bestPlayer} is the best player!{Environment.NewLine}He has scored {mostGoals} goals.");
+
+            }
+        }
+    }
+}
