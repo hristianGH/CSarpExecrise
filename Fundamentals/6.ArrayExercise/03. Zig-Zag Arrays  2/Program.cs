8e24d823f1ecb583643ea74640e67f6f18b40e4e
hristianGH <hristovhristian7@gmail.com>
2022-01-11 17:06:41 +0200

    3.Zigzag

---
+++ Fundamentals/6.ArrayExercise/03. Zig-Zag Arrays  2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _03._Zig_Zag_Arrays__2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string firstStorage = "";
+            string secondStorage = "";
+            for (int i =1; i <= n; i++)
+            {
+                string imput = Console.ReadLine();
+                string[] spliter = imput.Split();
+                if (i%2==0)
+                {
+                    firstStorage = firstStorage + " " + spliter[1];
+                    secondStorage = secondStorage + " " + spliter[0];
+
+                }
+                else
+                {
+                firstStorage = firstStorage + " " + spliter[0];
+                secondStorage = secondStorage + " " + spliter[1];
+
+                }
+
+            }
+            Console.WriteLine(firstStorage);
+            Console.WriteLine(secondStorage);
+
+        }
+    }
+}
---
+++ Fundamentals/6.ArrayExercise/03. Zig-Zag Arrays/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _03._Zig_Zag_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string firstStorage = "";
+            string secondStorage = "";
+            int counter = 0;
+
+
+            while (counter < n)
+            {
+                string first = Console.ReadLine();
+                firstStorage = firstStorage + " " + first;
+                counter++;
+                Console.WriteLine(counter);
+                // if (counter%2==0)
+                // {
+                //
+                // }
+                // string[] splitFirst = first.Split();
+                string second = Console.ReadLine();
+                secondStorage = secondStorage + " " + second;
+                //string[] splitSecond = second.Split();
+
+                counter++;
+                Console.WriteLine(counter);
+            }
+            Console.WriteLine(firstStorage);
+            Console.WriteLine(secondStorage);
+        }
+    }
+}
