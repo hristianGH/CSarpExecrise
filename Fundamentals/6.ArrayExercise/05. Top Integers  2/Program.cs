7a85ecbe0d2f2db99fcd282fdd96962b36ea61c0
hristianGH <hristovhristian7@gmail.com>
2022-01-11 17:12:28 +0200

    6.EqualSum

---
+++ Fundamentals/6.ArrayExercise/05. Top Integers  2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace _05._Top_Integers__2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            bool isBigger = true;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int current = numbers[i];
+
+                for (int y = i + 1; y < numbers.Length; y++)
+                {
+                    if (numbers[y] >= current)
+                    {
+                        isBigger = false;
+                        break;
+                    }
+                }
+                    if (isBigger)
+                    {
+                        Console.Write(current + " ");
+                    }
+                    isBigger = true;
+                
+            }
+        }
+    }
+}
---
+++ Fundamentals/6.ArrayExercise/06. Equal Sum/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace _06._Equal_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] imputNum = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            bool equalizer = false;
+            for (int i = 0; i < imputNum.Length; i++)
+            {
+                int sumRight = 0;
+                int sumLeft = 0;
+                for (int y = i + 1; y < imputNum.Length; y++)
+                {
+                    sumRight += imputNum[y];
+                }
+                for (int j = i - 1; j >= 0; j--)
+                {
+                    sumLeft += imputNum[j];
+                }
+
+               // Console.WriteLine(sumLeft + " " +  sumRight);
+               // Console.WriteLine(imputNum[i]);
+                if (sumLeft == sumRight )
+                {
+                Console.WriteLine(i);
+                    equalizer = true;
+                }
+            }
+            if (equalizer==false)
+            {
+                Console.WriteLine("no");
+            }
+        }
+    }
+}
