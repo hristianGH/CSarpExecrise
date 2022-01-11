ceb27bd71046f74e29aa35b0838e90a1172dec74
hristianGH <hristovhristian7@gmail.com>
2022-01-11 17:14:07 +0200

    8.MagicSum

---
+++ Fundamentals/6.ArrayExercise/07.Max Sequence/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace _07.Max_Sequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] imput = Console.ReadLine().Split();
+            int counterL = 0;
+            string numL = "";
+            for (int i = 0; i < imput.Length; i++)
+            {
+                int counter = 1;
+                string currentNum = imput[i];
+                for (int j = i+1; j < imput.Length; j++)
+                {
+                    if (currentNum==imput[j])
+                    {
+                        counter++;
+                    }
+                    else if (currentNum != imput[j])
+                    {
+                        break;
+                    }
+                    if (counter>counterL)
+                    {
+                        numL = currentNum;
+                        counterL = counter;
+                    }
+                }
+            }
+            for (int i = 0; i < counterL; i++)
+            {
+                Console.Write(numL + " ");
+            }
+        } 
+    }
+}
---
+++ Fundamentals/6.ArrayExercise/08. Magic Sum/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace _08._Magic_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] imputArr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int magicSum = int.Parse(Console.ReadLine());
+            for (int i = 0; i < imputArr.Length; i++)
+            {
+                for (int j = i+1; j < imputArr.Length; j++)
+                {
+                    if (imputArr[i]+imputArr[j]==magicSum)
+                    {
+                        Console.WriteLine(imputArr[i] + " "+ imputArr[j]);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
