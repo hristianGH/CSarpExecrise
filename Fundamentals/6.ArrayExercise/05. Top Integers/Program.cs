54acf0b05b01c54d49a017fa41513a17864e179d
hristianGH <hristovhristian7@gmail.com>
2022-01-11 17:07:31 +0200

    5.TopInteger

---
+++ Fundamentals/6.ArrayExercise/05. Top Integers/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace _05._Top_Integers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string imput = Console.ReadLine();
+            int[] numbers = imput.Split().Select(int.Parse).ToArray();
+            //string collector = "";
+            bool isBigger = true;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int current = numbers[i];
+                 
+                for (int y = 0; y < numbers.Length; y++)
+                {
+                    if (numbers[y]>=current)
+                    {
+                        isBigger = false;
+                        break;
+                    }
+                    if (isBigger)
+                    {
+                        Console.Write(current + " " );
+                    }
+                    isBigger = true;
+                }   
+            }
+          // string[] collectorSplit = collector.Split().Distinct().ToArray();
+          // 
+          // Console.WriteLine($"{string.Join(" ",collectorSplit)}");
+        }
+    }
+}
---
+++ Fundamentals/6.ArrayExercise/4. Array Rotation/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _4._Array_Rotation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] imputNumbers = Console.ReadLine().Split();
+            int rotationStarter = int.Parse(Console.ReadLine());
+
+            if (rotationStarter>imputNumbers.Length)
+            {
+                rotationStarter -= imputNumbers.Length;
+            }
+
+            for (int i = rotationStarter; i < imputNumbers.Length; i++)
+            {
+                Console.Write($"{imputNumbers[i]} ");
+
+            }
+
+                for (int y = 0; y < rotationStarter; y++)
+                {
+                    Console.Write($"{imputNumbers[y]} ");
+
+                }
+
+
+        }
+    }
+}
