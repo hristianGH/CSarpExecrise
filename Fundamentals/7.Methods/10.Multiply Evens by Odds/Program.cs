b2f1f3894e088e02fd66e4cf4fd488f572270f53
hristianGH <hristovhristian7@gmail.com>
2022-01-13 23:09:13 +0200

    5.Orders

---
+++ Fundamentals/7.Methods/10.Multiply Evens by Odds/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+
+namespace _10.Multiply_Evens_by_Odds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string imputDigits = Console.ReadLine();
+            char[] arrDigits = imputDigits.ToCharArray();
+             int odd = 0;
+             int even = 0;
+            int oddSum = 0;
+            int evenSum = 0;
+            for (int i = 0; i < arrDigits.Length; i++)
+            {
+                int stringToInteger = 0;
+                string charConverter ="" ; // arrDigits[i].ToString();
+                if (char.IsDigit(arrDigits[i]))
+                {
+                    charConverter = arrDigits[i].ToString();
+                    stringToInteger = int.Parse(charConverter);
+                }
+                Checker(out odd, out even,  stringToInteger);
+                oddSum += odd;
+                evenSum += even;
+            }
+            Console.WriteLine(oddSum*evenSum);
+        }
+
+       private static void Checker(out int oddSum, out int evenSum,  int stringToInteger)
+        {
+            oddSum = 0;
+            evenSum = 0;
+
+            if (stringToInteger % 2 == 0)
+
+            {
+                 evenSum += stringToInteger;
+            }
+            else
+            {
+                 oddSum += stringToInteger;
+            }
+              
+           // Console.WriteLine(oddSum + "" +evenSum);
+
+        }
+       
+    }
+}
---
+++ Fundamentals/7.Methods/5.Orders/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace _5.Orders
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int quantity = int.Parse(Console.ReadLine());
+          
+
+        }
+        public static double Order(string input,int quantity)
+        {
+            if (input.ToLower() == "coffee")
+            {
+                return 1.5 * quantity;
+            }
+            else if (input.ToLower() == "coke")
+            {
+                return 1.4 * quantity;
+            }
+            else if (input.ToLower() == "snacks")
+            {
+                return 2.0 * quantity;
+            }
+            return 1.0 * quantity;
+        }
+    }
+
+}
