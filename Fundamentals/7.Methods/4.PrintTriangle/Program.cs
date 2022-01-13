07f52e6cb88e92c075a80c7fd9485054fe1b5b05
hristianGH <hristovhristian7@gmail.com>
2022-01-13 22:56:40 +0200

    4.PrintTriangle

---
+++ Fundamentals/7.Methods/4.PrintTriangle/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _4.PrintTriangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int negative;
+            for (int i = 1; i <= n; i++)
+            {
+                for (int y = 1; y <= i && i<=n; y++)
+                {
+                    Console.Write(y+" ");
+                }
+                Console.WriteLine();
+            }
+            for (int i = n-1; i >= 1; i--)
+            {
+                for (int y = 1; y <= i; y++)
+                {
+                    Console.Write(y + " ");
+
+                }
+                Console.WriteLine();
+
+            }
+        }
+    }
+}
