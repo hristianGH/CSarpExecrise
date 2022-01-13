006494c8bd509eef6cde01ecc07dc1400a44e8ac
hristianGH <hristovhristian7@gmail.com>
2022-01-13 23:26:42 +0200

    Pow

---
+++ Fundamentals/7.Methods/8.MathPower/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _8.MathPower
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double fN = double.Parse(Console.ReadLine());
+            double sN = double.Parse(Console.ReadLine());
+            Power(fN, sN);
+
+        }
+        public static double Power(double fN,double sN)
+        {
+            return Math.Pow(fN,sN);
+        }
+    }
+}
