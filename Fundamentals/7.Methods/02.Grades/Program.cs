59e80b3d922a6ad1836bb7a0331d019a9d35e002
hristianGH <hristovhristian7@gmail.com>
2022-01-13 22:12:32 +0200

    Methods

---
+++ Fundamentals/7.Methods/02.Grades/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _02.Grades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double grade = double.Parse(Console.ReadLine());
+            GradeGrading(grade);
+        }
+
+          static void GradeGrading(double grade)
+        {
+            if (grade>2.99 && grade <3.49)
+            {
+                Console.WriteLine("Poor");
+            }
+            else if (grade >= 3.50 && grade < 4.49)
+                {
+                  Console.WriteLine("Good");
+                }
+            else if (grade >= 4.50 && grade < 5.49)
+            {
+                Console.WriteLine("Very good");
+            }
+            else if (grade >=5.50  )
+            {
+                Console.WriteLine("Exellent");
+            }
+            else  
+            {
+                Console.WriteLine("Fail");
+            }
+        }
+    }
+}
