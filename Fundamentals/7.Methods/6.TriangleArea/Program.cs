ed9f645038cd5779c1ec6d8545462d36b9241119
hristianGH <hristovhristian7@gmail.com>
2022-01-13 23:15:00 +0200

    Area

---
+++ Fundamentals/7.Methods/6.TriangleArea/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _6.TriangleArea
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+            Console.WriteLine(GetArea(width, height));
+
+        }
+        static double GetArea(double wd,double ht)
+        {
+            return wd * ht;
+        }
+    }
+}
---
+++ Fundamentals/7.Methods/7.RepeatString/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _7.RepeatString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+             string input = Console.ReadLine();
+            int n = int.Parse(Console.ReadLine());
+            Repeater(input, n);
+        }
+        public static string Repeater(string input,int n)
+        {
+            string pender = "";
+            for (int i = 0; i <= n; i++)
+            {
+                pender += input;
+            }
+            return pender;
+        }
+    }
+}
