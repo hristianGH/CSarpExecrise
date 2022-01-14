87fa4a6846a7386986b0d47799c00eca0c320bfe
hristianGH <hristovhristian7@gmail.com>
2022-01-14 22:07:00 +0200

    METHODS

---
+++ Fundamentals/8.MethodsExercise/01.Smallest Number of three/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _01.Smallest_Number_of_three
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNum = int.MaxValue;
+            int input = 0;
+            SmallestNum(input, firstNum);
+        }
+
+        private static void SmallestNum(int input,int firstNum)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                input = int.Parse(Console.ReadLine());
+                if (input < firstNum)
+                {
+                    firstNum = input;
+                }
+            }
+            Console.WriteLine(firstNum);
+        }
+    }
+}
