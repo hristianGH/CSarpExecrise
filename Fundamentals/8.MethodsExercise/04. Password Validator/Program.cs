d5916899002a39c9de78390e3b427ab1d4cf1b51
hristianGH <hristovhristian7@gmail.com>
2022-01-14 22:10:30 +0200

    Validator

---
+++ Fundamentals/8.MethodsExercise/04. Password Validator/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Linq;
+
+namespace _04._Password_Validator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string password = Console.ReadLine();
+            int asciiFromChar = 0;
+
+            int wat = password.Length;
+            int errors = 0;
+            char[] passwordToCharacters = password.ToCharArray();
+
+            IsPasswordLongEnough(wat, ref errors);
+
+            DoesPasswordContainTwoDigits(passwordToCharacters, ref errors);
+
+            OnlyLettersAndDigits(passwordToCharacters, ref errors);
+
+            ErrorChecker(errors);
+           
+        }  
+
+        private static void ErrorChecker(int errors)
+        {
+            if (errors < 1)
+            {
+                Console.WriteLine("Password is valid");
+
+            }
+        }
+
+        private static void OnlyLettersAndDigits(char[] passwordToCharacters, ref int errors)
+        {
+            int asciiFromChar = 0;
+
+            for (int i = 0; i < passwordToCharacters.Length; i++)
+            {
+                asciiFromChar = passwordToCharacters[i];
+                if (asciiFromChar < 48 || asciiFromChar > 57 && asciiFromChar < 65 || asciiFromChar > 90 && asciiFromChar < 97 || asciiFromChar > 122)
+                {
+
+                    Console.WriteLine("Password must consist of only letters and digits ");
+                    errors++;
+                    Environment.Exit(0);
+                }
+
+            }
+        }
+
+        private static void DoesPasswordContainTwoDigits(char[] passwordToCharacters, ref int errors)
+        {
+            int digitCounter = 0;
+
+            for (int i = 0; i < passwordToCharacters.Length; i++)
+            {
+
+                if (Char.IsNumber(passwordToCharacters[i]) == true)
+                {
+                    digitCounter++;
+                }
+
+            }
+
+            if (digitCounter < 2)
+            {
+                Console.WriteLine("Password must have at least 2 digits ");
+                errors++;
+            }
+        }
+
+        private static int IsPasswordLongEnough(int wat, ref int errors)
+        {
+            if (wat < 6 || wat > 10)
+            {
+                Console.WriteLine("Password must be between 6 and 10 characters");
+                return errors++;
+            }
+            return wat;
+
+        }
+    }
+}
---
+++ Fundamentals/8.MethodsExercise/05.AddAndSubtract/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _05.AddAndSubtract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int imput = 0 ;
+           
+            InputToSum(imput);
+        }
+
+        private static void InputToSum(int imput  )
+        {
+            int[] imputArr = new int[3];
+            for (int i = 0; i < 3; i++)
+            {
+                imput = int.Parse(Console.ReadLine());
+                imputArr[i] = imput;
+            }
+            imput = (imputArr[0] + imputArr[1]) - imputArr[2];
+             Console.WriteLine(imput);
+        }
+    }
+}
