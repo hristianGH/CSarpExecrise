ddd6ff58f826fabb6b6c8c03947907f20fbe7648
hristianGH <hristovhristian7@gmail.com>
2022-01-14 22:08:20 +0200

    Toascii

---
+++ Fundamentals/8.MethodsExercise/03.CharecterToAscii/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _03.CharecterToAscii
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char inputChar = Convert.ToChar(Console.ReadLine());
+            char inputTheSecond = Convert.ToChar(Console.ReadLine()); ;
+            int toAscii = inputChar;
+            int toAsciiTheSecondSonOfTheDragonFromTheVillageOfThe = inputTheSecond;
+            ToIntegerFromChar(toAscii, toAsciiTheSecondSonOfTheDragonFromTheVillageOfThe, inputChar, inputTheSecond);
+            InRangeLoop(inputChar, inputTheSecond, toAscii, toAsciiTheSecondSonOfTheDragonFromTheVillageOfThe);
+        }
+
+        private static void InRangeLoop(char inputChar, char inputTheSecond, int toA, int toAtwo)
+        {
+         
+            for (int i = toA + 1; i < toAtwo; i++)
+            {
+                char charWho = (char)i;
+                Console.Write(charWho + " "  );
+            }
+        }
+        private static void ToIntegerFromChar(int a, int b, char c, char d)
+        {
+            int toAscii = c;
+            int toAsciiTheSecondSonOfTheDragonFromTheVillageOfThe = d;
+        }
+    }
+}
