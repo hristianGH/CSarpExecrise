3563d53e000ce919b9d84a96524524b7e0679095
hristianGH <hristovhristian7@gmail.com>
2022-01-17 22:52:22 +0200

    3.Merging

---
+++ Fundamentals/10.Lists/02.gTrick/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.gTrick
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> input = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse)
+                .ToList();
+            int midpoint = input.Count / 2;
+            for (int i = 0; i < midpoint; i++)
+            {
+                input[i] += input[input.Count - 1];
+                input.RemoveAt(input.Count - 1);
+            }
+            Console.WriteLine(string.Join(" ", input));
+
+        }
+    }
+}
---
+++ Fundamentals/10.Lists/03. MergingLists/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._MergingLists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+            List<int> inputTheTwo = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+            List<int> saveList = new List<int>();
+            int counterOne = input.Count - 1;
+            int counterTwo = inputTheTwo.Count - 1;
+            int indexOne = 0;
+            int indexTwo = 0;
+
+            while (counterOne != 0 || counterTwo != 0)
+            {
+                if (counterOne > 0)
+                {
+                    saveList.Add(input[indexOne]);
+                    indexOne++;
+                    counterOne--;
+                }
+                if (counterTwo > 0)
+                {
+                    saveList.Add(inputTheTwo[indexTwo]);
+                    indexTwo++;
+                    counterTwo--;
+                }
+            }
+
+
+            Console.WriteLine(string.Join(" ", saveList));
+        }
+    }
+}
