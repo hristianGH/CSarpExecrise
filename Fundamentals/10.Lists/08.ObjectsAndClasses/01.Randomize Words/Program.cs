be1a5bcd4dd9be0546c0c8d5f80d07d60fb33244
hristianGH <hristovhristian7@gmail.com>
2022-01-20 21:06:20 +0200

    1.RndWords

---
+++ Fundamentals/10.Lists/08.ObjectsAndClasses/01.Randomize Words/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace _01.Randomize_Words
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).ToArray();
+            for (int i = 0; i < input.Length; i++)
+            {
+                Random roll = new Random();
+                System.Threading.Thread.Sleep(10);
+                string holder = input[i];
+                int swap = roll.Next(0,input.Length);
+                input[i] = input[swap];
+                input[swap] = holder;
+            }
+            Console.WriteLine(string.Join(Environment.NewLine, input));
+        }
+    }
+}
