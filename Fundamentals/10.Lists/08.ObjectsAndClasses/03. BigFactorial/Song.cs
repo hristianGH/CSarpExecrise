8f6b91ca752ef3d052e0fade4e0bd60c84faae1f
hristianGH <hristovhristian7@gmail.com>
2022-01-20 21:07:30 +0200

    BigFactorial

---
+++ Fundamentals/10.Lists/08.ObjectsAndClasses/03. BigFactorial/Song.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _03._BigFactorial
+{
+    class Song
+    {
+        public string Type { get; set; }
+        public string Name { get; set; }
+
+
+    }
+}
