88c68d7c95d5eade44f57c1e325497035789aec3
hristianGH <hristovhristian7@gmail.com>
2022-01-24 22:45:51 +0200

    BigF

---
+++ Fundamentals/12.ObjectsAndClasses/03. BigFactorial/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._BigFactorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<Song> playList = new List<Song>();
+            for (int i = 0; i < n; i++)
+            {
+                string[] inputSong = Console.ReadLine().Split('_').ToArray();
+                string type = inputSong[0];
+                string songName = inputSong[1];
+                Song song = new Song();
+                song.Type = type;
+                song.Name = songName;
+             
+                playList.Add(song);
+            }
+            string command = Console.ReadLine();
+            List<Song> filtered =playList.Where(x=>x.Type==command).ToList();
+            if (command=="all")
+            {
+                Console.WriteLine(string.Join(Environment.NewLine,playList));
+            }
+            foreach (Song song in filtered)
+            {
+                if (song.Type==command)
+                {
+                    Console.WriteLine(song.Name);
+                }
+            }
+            
+        }
+    }
+}
