c541dfb354745a74d6ac36ffee16a40572aec637
hristianGH <hristovhristian7@gmail.com>
2022-01-27 13:59:54 +0200

    ObjExercise

---
+++ Fundamentals/13.ObjectsAndClasses/1.Advertisement Message/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+
+
+namespace _1.Advertisement_Message
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Messege> construct = new List<Messege>();
+            Random rnd = new Random();
+            int rolls = int.Parse(Console.ReadLine());
+            int[] rndStorage = new int[rolls * 4];
+
+            string[] phrases = { "Excellent product.", "Such a great product.", "I always use that product.", "Best product of its category.",
+                    "Exceptional product.",
+                    "I can’t live without this product.",
+                    "Excellent product.",
+                    "I always use that product."};
+            string[] events = { "Now I feel good.", "I have succeeded with this product.", "Makes miracles. I am happy of the results!",
+                    "I cannot believe but now I feel awesome.",
+                    "Try it yourself, I am very satisfied.",
+                    "I feel great!",
+                    "I feel great!",
+                    "Makes miracles. I am happy of the results!"};
+            string[] authors = { "Diana", "Petya", "Stella", "Elena", "Katya", "Iva", "Annie", "Eva" };
+            string[] cities = { "Burgas", "Sofia", "Plovdiv", "Varna", "Ruse", "Sofia", "Plovdiv", "Varna" };
+            for (int i = 0; i < rndStorage.Length; i++)
+            {
+                rndStorage[i] = rnd.Next(phrases.Length - 1);
+
+            }
+            for (int i = 0; i < rolls; i++)
+            {
+                Messege text = new Messege()
+                {
+                    Phrases = phrases[rndStorage[i]],
+                    Events = events[rndStorage[i + 1]],
+                    Authors=authors[rndStorage[i+2]],
+                    Cities=cities[rndStorage[i+3]]
+                };
+                construct.Add(text);
+            }
+            foreach (var text in construct)
+            {
+
+                Console.WriteLine($"{string.Join(" ", text.Phrases, text.Events)} {(text.Authors)} - {(text.Cities)}  " );
+            }
+          
+        }
+    }
+    class Messege
+    {
+        public string Phrases { get; set; }
+        public string Events { get; set; }
+        public string Authors { get; set; }
+        public string Cities { get; set; }
+    }
+}
---
+++ Fundamentals/13.ObjectsAndClasses/2.Articles/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+
+namespace _2.Articles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Article> article = new List<Article>();
+            string[] contents = Console.ReadLine().Split(',', StringSplitOptions.RemoveEmptyEntries);
+            string title = contents[0];
+            string content = contents[1];
+            string author = contents[2];
+            Article text = new Article(title, content, author);
+            article.Add(text);
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string[] command = Console.ReadLine().Split(": ");
+                switch (command[0])
+                {
+                    case "Edit":
+                        text.Content = command[1];
+                        break;
+                    case "ChangeAuthor":
+                        text.Author = command[1];
+                        
+                        break;
+                    case "Rename":
+                        text.Title = command[1];
+                        break;
+
+                }
+            }
+            foreach (var item in article)
+            {
+
+                Console.WriteLine(item.ToString());
+            }
+        }
+    }
+    class Article
+    {
+        public Article(string a, string b, string c)
+        {
+            Title = a;
+            Content = b;
+            Author = c;
+        }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string Author { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Title} - {Content}: {Author}";
+        }
+    }
+
+
+}
+
