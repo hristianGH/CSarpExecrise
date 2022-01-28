ac960086bb071514ca30e132c0656bf5fb0887d4
hristianGH <hristovhristian7@gmail.com>
2022-01-28 12:19:00 +0200

    2.OddOcurances

---
+++ Fundamentals/14.AssociativeArrays/02.OddOccurrences/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _02.OddOccurrences
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arr = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            Dictionary<string, int> dicdic = new Dictionary<string, int>();
+            foreach (var item in arr)
+            {
+                string toLower = item.ToLower();
+                if (dicdic.ContainsKey(toLower))
+                {
+                    dicdic[toLower]++;
+                }
+                else
+                {
+                    dicdic.Add(toLower, 1);
+                }
+            }
+            StringBuilder sb = new StringBuilder();
+            foreach (var item in dicdic)
+            {
+                if (item.Value % 2 != 0)
+                {
+                    Console.Write($"{item.Key} ");
+                }
+            }
+
+        }
+    }
+}
