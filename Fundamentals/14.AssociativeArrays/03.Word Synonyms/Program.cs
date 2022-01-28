4b1f625d023a17fd3443684a067b0d665f7c8e92
hristianGH <hristovhristian7@gmail.com>
2022-01-28 12:25:09 +0200

    Update

---
+++ Fundamentals/14.AssociativeArrays/03.Word Synonyms/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _03.Word_Synonyms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> dicdic = new Dictionary<string, List<string>>();
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string inputOne = Console.ReadLine();
+                string inputTwo = Console.ReadLine();
+                if (dicdic.ContainsKey(inputOne))
+                {
+                    dicdic[inputOne].Add(inputTwo);
+                }
+                else
+                {
+                    dicdic.Add(inputOne, new List<string>());
+                    dicdic[inputOne].Add(inputTwo);
+
+                }
+
+
+            }
+            foreach (var item in dicdic)
+            {
+                Console.Write($"{item.Key} - {string.Join(", ",item.Value)}");
+                
+                Console.WriteLine();
+            }
+        }
+    }
+}
+ 
