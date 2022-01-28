2a1af04f4fc94acaf7c7817ec23392b24d3868d3
hristianGH <hristovhristian7@gmail.com>
2022-01-28 12:15:22 +0200

    AssociativeArrays

---
+++ Fundamentals/14.AssociativeArrays/01.CountRealNumbers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.CountRealNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            Dictionary<int, int> num = new Dictionary<int, int>();
+            foreach (var numer in arr)
+            {
+                if (num.ContainsKey(numer))
+                {
+                    num[numer]++;
+                }
+                else
+                {
+                    num.Add(numer, 1);
+                }
+            }
+            foreach (var item in num)
+            {
+            Console.WriteLine($"{item.Key} -> {item.Value}");
+
+            }
+        }
+    }
+}
