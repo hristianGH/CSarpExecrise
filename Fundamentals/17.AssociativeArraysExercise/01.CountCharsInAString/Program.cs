88d68db3e58fec23ec67fbbbf48aaca64f3c0960
hristianGH <hristovhristian7@gmail.com>
2022-01-30 22:23:15 +0200

    AssociativeArray

---
+++ Fundamentals/17.AssociativeArraysExercise/01.CountCharsInAString/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01.CountCharsInAString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<char, int> dicdic = new Dictionary<char, int>();
+            string[] input = Console.ReadLine().Split();
+            for (int i = 0; i < input.Length; i++)
+            {
+                char[] inputToChar = input[i].ToCharArray();
+                for (int y = 0; y < inputToChar.Length; y++)
+                {
+                    if (dicdic.ContainsKey(inputToChar[y]))
+                    {
+                        dicdic[inputToChar[y]]++;
+                    }
+                    else
+                    {
+                        dicdic.Add(inputToChar[y], 1);
+                    }
+                }
+            }
+            foreach (var item in dicdic)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+    }
+}
---
+++ Fundamentals/17.AssociativeArraysExercise/02.AMinerTask/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _02.AMinerTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> dicdic = new Dictionary<string, int>();
+            string input = Console.ReadLine();
+            while (input!="stop")
+            {
+                int n = int.Parse(Console.ReadLine());
+                if (dicdic.ContainsKey(input))
+                {
+                    dicdic[input] += n;
+                }
+                else
+                {
+                    dicdic.Add(input, n);
+                }
+                input = Console.ReadLine();
+            }
+                foreach (var item in dicdic)
+                {
+                    Console.WriteLine($"{item.Key} -> {item.Value}");
+                }
+        }
+    }
+}
