5210f94023331fb3dafb5a9e8ec4b960447843de
hristianGH <hristovhristian7@gmail.com>
2022-01-31 12:11:56 +0200

    DigitLetter and MultiplyBigInt

---
+++ Fundamentals/18.TextProcesingAndExercise/05.DigitLetterOther/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace _05.DigitLetterOther
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            StringBuilder numbers = new StringBuilder();
+            input.Where(x => char.IsDigit(x)).ToList().ForEach(x => Console.Write(x));
+            Console.WriteLine();
+            input.Where(x => char.IsLetter(x)).ToList().ForEach(x => Console.Write(x));
+            Console.WriteLine();
+            input.Where(x => !char.IsLetterOrDigit(x)).ToList().ForEach(x => Console.Write(x));
+
+
+        }
+    }
+}
---
+++ Fundamentals/18.TextProcesingAndExercise/05.MultiplyBigN/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+namespace _05.MultiplyBigN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+        }
+    }
+}
