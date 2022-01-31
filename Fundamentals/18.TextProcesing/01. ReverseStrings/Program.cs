c0177526b9e5ce9cbacfea60be80dbee2c0a9cd3
hristianGH <hristovhristian7@gmail.com>
2022-01-31 10:48:08 +0200

    TextProcesing

---
+++ Fundamentals/18.TextProcesing/01. ReverseStrings/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace _01._ReverseStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string input = Console.ReadLine();
+            while (input != "end")
+            {
+                string output = "";
+                input.ToCharArray().Reverse().Where(x => x != null).ToList().ForEach(x => output += x);
+                Console.WriteLine($"{input} = {output}");
+                output = "";
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Fundamentals/18.TextProcesing/01.ValidUser/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.ValidUser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries);
+            List<string> names = new List<string>();
+            names.AddRange(input);
+            foreach (var item in names)
+            {
+                bool isTrue= item.All(c => Char.IsLetterOrDigit(c) || c == '_'||c=='-');
+                if (isTrue==true&&item.Length>3&&item.Length<16)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+                
+
+        }
+    }
+}
