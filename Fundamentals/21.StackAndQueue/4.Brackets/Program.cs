9dbce8895e013e4afbf385650de5f01642328ff6
hristianGH <hristovhristian7@gmail.com>
2022-02-02 10:28:11 +0200

    Brakets

---
+++ Fundamentals/21.StackAndQueue/4.Brackets/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _4.Brackets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Stack<int> brackets = new Stack<int>();
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i]=='(')
+                {
+                    brackets.Push(i);
+                }
+                if (input[i]==')')
+                {
+                    int end =i-brackets.Peek();
+                    Console.WriteLine(input.Substring(brackets.Pop(),end+1));
+                }
+
+            }
+
+             
+        }
+    }
+}
