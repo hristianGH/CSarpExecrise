7acc6ce6958f3890c67a01d7a4cda76dc4dc1ef8
hristianGH <hristovhristian7@gmail.com>
2022-02-02 10:28:46 +0200

    EvenNum

---
+++ Fundamentals/21.StackAndQueue/5.EvenNum/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _5.EvenNum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> even = new Queue<int>(Console.ReadLine().Split().Select(int.Parse).Where(x=>x%2==0));
+            Console.WriteLine(string.Join(", ",even));
+        }
+    }
+}
