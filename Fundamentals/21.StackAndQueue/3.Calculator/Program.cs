57386a5de293ee774c0a8b6a0e6fc3f3fb20210e
hristianGH <hristovhristian7@gmail.com>
2022-02-02 10:26:26 +0200

    CalcWithOnlyPlus

---
+++ Fundamentals/21.StackAndQueue/3.Calculator/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _3.Calculator
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            {
+                int sumNum = 0;
+                Stack<string> sum = new Stack<string>(Console.ReadLine().Split().Reverse());
+                sumNum = int.Parse(sum.Pop());
+                while (sum.Count>1)
+                {
+                    if (sum.Pop()=="+")
+                    {
+                        sumNum += int.Parse(sum.Pop());
+                    }
+                    else
+                    {
+                        
+                        sumNum -= int.Parse(sum.Pop());
+
+                    }
+                }
+                Console.WriteLine(sumNum);
+                 
+            }
+
+        }
+    }
+}
+
