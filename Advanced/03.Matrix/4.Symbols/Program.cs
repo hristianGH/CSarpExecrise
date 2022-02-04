f9cd0750a6855890f4e3d9624eb3ab0a53ff2cb4
hristianGH <hristovhristian7@gmail.com>
2022-02-04 10:53:16 +0200

    Symbols

---
+++ Advanced/03.Matrix/4.Symbols/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace _4.Symbols
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool Isfound = false;
+            int n = int.Parse(Console.ReadLine());
+            char[,] matrix = new char[n, n];
+            for (int i = 0; i < n; i++)
+            {
+                char[] arr = Console.ReadLine().ToCharArray();
+                for (int y = 0; y < n; y++)
+                {
+                    matrix[i, y] = arr[y];
+                }
+            }
+            char symbol = char.Parse(Console.ReadLine());
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int y = 0; y < matrix.GetLength(1); y++)
+                {
+                    if (matrix[i,y]==symbol)
+                    {
+                        Console.WriteLine($"({i}, {y})");
+                        Isfound = true;
+                    }
+                    
+                }
+                
+            }
+            if (Isfound==false)
+            {
+                Console.WriteLine($"{symbol} does not occur in the matrix");
+            }
+        }
+    }
+}
