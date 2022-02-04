6a8c8d8e80ff1da613dedb82c08cd450638711c5
hristianGH <hristovhristian7@gmail.com>
2022-02-04 10:52:49 +0200

    2,3

---
+++ Advanced/03.Matrix/2.SumCollums/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace _2.SumCollums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] range = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+            int[,] arrayRange = new int[range[0], range[1]];
+            for (int i = 0; i < range[0]; i++)
+            {
+                string[] numbers = Console.ReadLine().Split();
+                for (int y = 0; y < range[1]; y++)
+                {
+                    arrayRange[i, y] = int.Parse(numbers[y]);
+                }
+            }
+            for (int i = 0; i < range[1]; i++)
+            {
+                int sum = 0;
+                for (int y = 0; y < range[0]; y++)
+                {
+                    sum += arrayRange[y, i];
+                }
+                    Console.WriteLine(sum);
+            }
+        }
+    }
+}
---
+++ Advanced/03.Matrix/3.Diagonal/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _3.Diagonal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int n = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[n, n];
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+                for (int y = 0; y < n; y++)
+                {
+                    matrix[i, y] = int.Parse(input[y]); 
+                }
+            }
+            n = 0;
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                n += matrix[i,i];
+            }
+            Console.WriteLine(n);
+        }
+    }
+}
