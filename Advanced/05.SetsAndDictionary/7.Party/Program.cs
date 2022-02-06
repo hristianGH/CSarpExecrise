7506e08fdd88eb1589433f05320cf50fea48462c
hristianGH <hristovhristian7@gmail.com>
2022-02-06 11:18:30 +0200

    Party

---
+++ Advanced/05.SetsAndDictionary/7.Party/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _7.Party
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            HashSet<string> normal = new HashSet<string>();
+            HashSet<string> vip = new HashSet<string>();
+            string input = Console.ReadLine();
+            while (input != "PARTY" && input != "END")
+            {
+                if (char.IsDigit(input[0]))
+                {
+                    vip.Add(input);
+                }
+                else
+                {
+                    normal.Add(input);
+                }
+                input = Console.ReadLine();
+            }
+            if (input == "PARTY")
+            {
+                input = Console.ReadLine();
+                while (input != "END")
+                {
+                    if (normal.Contains(input))
+                    {
+                        normal.Remove(input);
+                    }
+                    if (vip.Contains(input))
+                    {
+                        vip.Remove(input);
+                    }
+                    input = Console.ReadLine();
+                }
+            }
+            count = vip.Count + normal.Count;
+            Console.WriteLine(count);
+            if (vip.Count > 0)
+            {
+                foreach (var item in vip)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+            if (normal.Count > 0)
+            {
+
+                foreach (var item in normal)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+
+        }
+    }
+}
