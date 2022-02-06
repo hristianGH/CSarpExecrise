cef8a1940493103c7a3738f1576dfebb617f8ff5
hristianGH <hristovhristian7@gmail.com>
2022-02-06 11:17:19 +0200

    Counter

---
+++ Advanced/05.SetsAndDictionary/1.Counter/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _1.Counter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> count = new Dictionary<string, int>();
+            foreach (var item in Console.ReadLine().Split())
+            {
+                if (count.ContainsKey(item))
+                {
+                    count[item]++;
+                }
+                else
+                {
+                    count.Add(item, 1);
+                }
+            }
+            foreach (var item in count)
+            {
+                Console.WriteLine($"{item.Key} - {item.Value} times");
+            }
+
+        }
+    }
+}
