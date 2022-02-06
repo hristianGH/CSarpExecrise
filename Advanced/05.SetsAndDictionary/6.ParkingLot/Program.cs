5bd7a82583601b1845e22ea3efce6f20db2c974c
hristianGH <hristovhristian7@gmail.com>
2022-02-06 11:18:17 +0200

    Parking

---
+++ Advanced/05.SetsAndDictionary/6.ParkingLot/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+namespace _6.ParkingLot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> cars = new HashSet<string>();
+            string[] arr = Console.ReadLine().Split(", ");
+            while (arr[0]!="END")
+            {
+                if (arr[0]=="IN")
+                {
+                    cars.Add(arr[1]);
+                }
+                else
+                {
+                    cars.Remove(arr[1]);
+                }
+                arr = Console.ReadLine().Split(", ");
+            }
+            foreach (var item in cars)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
