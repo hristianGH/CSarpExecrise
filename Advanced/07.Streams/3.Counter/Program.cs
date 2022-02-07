183ece4b02f388e7c70369a492305ea05816f20b
hristianGH <hristovhristian7@gmail.com>
2022-02-07 16:54:24 +0200

    Counter

---
+++ Advanced/07.Streams/3.Counter/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace _3.Counter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> dicdic = new Dictionary<string, int>();
+            using (StreamReader toFind = new StreamReader(Path.Combine("data", "textfile1.txt")))
+            {
+                string[] words = toFind.ReadLine().Split();
+                foreach (var item in words)
+                {
+                    dicdic.Add(item, 1);
+                }
+            }
+            using (StreamReader toFind = new StreamReader(Path.Combine("data", "textfile2.txt")))
+            {
+                string reader = toFind.ReadLine();
+                    string[] words = reader.Split();
+                while (reader!=null)
+                {
+                    foreach (var itemTwo in words)
+                    {
+                        if (dicdic.ContainsKey(itemTwo.ToLower()))
+                        {
+                            dicdic[itemTwo.ToLower()]++;
+                        }
+                    }
+                      reader = toFind.ReadLine();
+                    if (reader!=null)
+                    {
+                      words = reader.Split();
+                    }
+                }
+            }
+            using (FileStream file = new FileStream(Path.Combine("data","textfileOut.txt"),FileMode.OpenOrCreate))
+            {
+                using (StreamWriter writer = new StreamWriter(file))
+                {
+                    foreach (var item in dicdic)
+                    {
+                        writer.WriteLine($"{item.Key} - {item.Value}");  
+                    }
+                }
+            }
+        }
+    }
+}
