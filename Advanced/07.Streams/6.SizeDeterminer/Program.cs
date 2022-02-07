45041f0cc062f640173468d024aeda8c9ebb4fc9
hristianGH <hristovhristian7@gmail.com>
2022-02-07 16:55:21 +0200

    SizeDeterminer

---
+++ Advanced/07.Streams/6.SizeDeterminer/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace _6.SizeDeterminer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                int index = 0;
+                string[] path = Path.GetFullPath(Environment.CurrentDirectory).Split(Path.DirectorySeparatorChar);
+            {
+              
+                for (int i = 0; i < path.Length; i++)
+                {
+                    if (path[i] == "repos")
+                    {
+                        index = i;
+                    }
+                }
+            }
+            string pathing = "";
+            for (int i = 0; i < index; i++)
+            {
+              pathing=  Path.Combine(pathing,path[i]);
+            }
+            FileInfo lenght = new FileInfo(pathing);
+            
+            Console.WriteLine(lenght.);
+        }
+    }
+}
