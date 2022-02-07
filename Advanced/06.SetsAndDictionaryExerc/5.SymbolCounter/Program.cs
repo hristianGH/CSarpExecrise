21239a57d65effb995b488b1f31ce3bd9c8acd65
hristianGH <hristovhristian7@gmail.com>
2022-02-07 10:32:36 +0200

    SymbolCount

---
+++ Advanced/06.SetsAndDictionaryExerc/5.SymbolCounter/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _5.SymbolCounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            SortedDictionary<char, int> dicdic = new SortedDictionary<char, int>();
+
+            foreach (var item in input)
+            {
+                if (dicdic.ContainsKey(item))
+                {
+                    dicdic[item]++;
+                }
+                else
+                {
+                    dicdic.Add(item, 1);
+                }
+            }
+
+            foreach (var item in dicdic)
+            {
+                Console.WriteLine($"{item.Key}: {item.Value} time/s");
+            }
+        }
+    }
+}
