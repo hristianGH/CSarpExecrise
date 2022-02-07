79d84c5b6d9f272771c84b66aad9aa02810a81cb
hristianGH <hristovhristian7@gmail.com>
2022-02-07 10:32:10 +0200

    Table

---
+++ Advanced/06.SetsAndDictionaryExerc/3.PeriodicTable/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _3.PeriodicTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedSet<string> set = new SortedSet<string>();
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+                foreach (var item in input)
+                {
+                    set.Add(item);
+
+                }
+            }
+         
+            foreach (var item in set)
+            {
+                Console.Write($"{item} ");
+            }
+        }
+    }
+}
