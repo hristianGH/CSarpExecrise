c29acdaeb73a5d1255f99d609316dd147cce7e49
hristianGH <hristovhristian7@gmail.com>
2022-02-07 10:31:55 +0200

    SetElements

---
+++ Advanced/06.SetsAndDictionaryExerc/2.SetElements/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _2.SetElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nOne = Console.ReadLine().Split();
+             
+            HashSet<int> setOne = new HashSet<int>(int.Parse(nOne[0]));
+            for (int i = 0; i < int.Parse(nOne[0]); i++)
+            {
+                setOne.Add(int.Parse(Console.ReadLine()));
+            }
+            
+            HashSet<int> setTwo = new HashSet<int>(int.Parse(nOne[1]));
+            for (int i = 0; i < int.Parse(nOne[1]); i++)
+            {
+                setTwo.Add(int.Parse(Console.ReadLine()));
+            }
+            if (setOne.Count>=setTwo.Count)
+            {
+                foreach (var item in setOne)
+                {
+                    if (setTwo.Contains(item))
+                    {
+                        Console.Write($"{item} ");
+                    }
+                }
+            }
+            else
+            {
+                foreach (var item in setTwo)
+                {
+                    if (setTwo.Contains(item))
+                    {
+                        Console.Write($"{item} ");
+                    }
+                }
+            }
+        }
+    }
+}
