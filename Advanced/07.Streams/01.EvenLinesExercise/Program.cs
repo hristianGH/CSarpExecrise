47ebcf61f4153423652e6a2c3ab44ef3c13d8d36
hristianGH <hristovhristian7@gmail.com>
2022-02-07 16:53:19 +0200

    EvenLines

---
+++ Advanced/07.Streams/01.EvenLinesExercise/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _01.EvenLinesExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader reader = new StreamReader("../../../input.txt"))
+            {
+                string line = string.Empty;
+
+                int row = 0;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    if (row % 2 == 0)
+                    {
+                        Regex pattern = new Regex("[-,.!?]");
+
+                        line = pattern.Replace(line, "@");
+
+                        var lineArray = line.Split().ToArray().Reverse();
+
+                        Console.WriteLine(string.Join(" ", lineArray));
+                    }
+
+                    row++;
+                }
+            }
+        }
+    }
+}
