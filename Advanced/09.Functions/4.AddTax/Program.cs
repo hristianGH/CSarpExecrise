68ad0e113a368ea2d92a94a610fae09136ea6840
hristianGH <hristovhristian7@gmail.com>
2022-02-09 10:08:32 +0200

    AddVat

---
+++ Advanced/09.Functions/4.AddTax/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _4.AddTax
+{
+    public delegate decimal AddVat(decimal x);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arr = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries);
+            AddVat vatPlus20 = x =>
+            {
+                return x * 1.20M;
+            };
+            foreach (var item in arr)
+            {
+                Console.WriteLine($"{vatPlus20(decimal.Parse(item)):f2}");
+            }
+        }
+    }
+}
