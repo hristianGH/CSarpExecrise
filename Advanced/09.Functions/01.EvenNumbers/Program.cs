199cc79c995ec0de7085ddf598f4dcbd41da32a3
hristianGH <hristovhristian7@gmail.com>
2022-02-09 10:08:00 +0200

    EvenNums

---
+++ Advanced/09.Functions/01.EvenNumbers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.EvenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).
+                Select(int.Parse)
+                .Where(n => n % 2 == 0)
+                .OrderBy(n => n)
+                .ToList();
+
+            Console.WriteLine(string.Join(", ", nums));
+                
+        }
+    }
+}
