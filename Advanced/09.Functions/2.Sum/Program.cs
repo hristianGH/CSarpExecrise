c83e5d6a095801adf9f55a01a5e15b938540a418
hristianGH <hristovhristian7@gmail.com>
2022-02-09 10:08:15 +0200

    Sum

---
+++ Advanced/09.Functions/2.Sum/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _2.Sum
+{
+    class Program
+    {
+        public delegate int EachNumSum(int x, int y);
+             
+        static void Main(string[] args)
+        {
+            string[] arr = Console.ReadLine().Split(", ",StringSplitOptions.RemoveEmptyEntries);
+             
+            EachNumSum xPlusY = (int x, int result) =>
+            {
+                result += x;
+                return result;
+            };
+            
+
+            Console.WriteLine($"{arr.Length}");
+            Console.WriteLine(Sum(arr,xPlusY));
+        }
+
+        private static int Sum(string[] arr,EachNumSum xPlusY)
+        {
+            int result = 0;
+            foreach (var item in arr)
+            {
+               result= xPlusY(int.Parse(item), result);
+            }
+            return result  ;
+        }
+
+    }
+}
