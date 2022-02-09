78624782b5614da61996ffa31ca831c2168b4d58
hristianGH <hristovhristian7@gmail.com>
2022-02-09 10:08:21 +0200

    Count

---
+++ Advanced/09.Functions/3.CountUpper/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _3.CountUpper
+{
+    class Program
+    {
+        public delegate void IsFirstCharUpper(string[] txt);
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries);
+            IsFirstCharUpper ck = (string[] x) =>
+            {
+                foreach (var item in input)
+                {
+                    if (char.IsUpper(item[0]))
+                    {
+                        Console.WriteLine(item);
+                    }
+                }
+            };
+            ck(input);
+              
+             
+        }
+    }
+}
