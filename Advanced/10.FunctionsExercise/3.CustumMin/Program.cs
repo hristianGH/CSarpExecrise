1e7609958c33b5a815225b356a0c75b224b807ed
hristianGH <hristovhristian7@gmail.com>
2022-02-10 12:09:56 +0200

    Min

---
+++ Advanced/10.FunctionsExercise/3.CustumMin/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _3.CustumMin
+{
+    public delegate void MIN(string[] x);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MIN min = x =>
+            {
+                int maxvalue = int.MaxValue;
+                int minvalue = 0;
+                foreach (var item in x)
+                {
+                    if (int.Parse(item) < maxvalue)
+                    {
+                        maxvalue = int.Parse(item);
+                        minvalue = maxvalue;
+                    }
+                }
+                Console.WriteLine(minvalue); ;
+            };
+            min(Console.ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries));
+        }
+
+    }
+}
+    
