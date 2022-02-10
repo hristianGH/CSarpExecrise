fd6dcad426092f221145823bc637f89afa39c622
hristianGH <hristovhristian7@gmail.com>
2022-02-10 12:09:38 +0200

    ActionForeach

---
+++ Advanced/10.FunctionsExercise/1.ActionForeach/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _1.ActionForeach
+{
+    class Program
+    {
+            delegate void Each(string x);
+        
+        static void Main(string[] args)
+        {
+            Each each = x =>
+            {
+               
+                foreach (var item in x.Split())
+                {
+                      Console.WriteLine(item);
+                }
+            };
+            each(Console.ReadLine());
+        }
+    }
+}
