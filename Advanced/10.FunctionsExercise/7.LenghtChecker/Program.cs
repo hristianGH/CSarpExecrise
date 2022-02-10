f30c31962cbee0e41c02c4a8b4c2b002f914fc8f
hristianGH <hristovhristian7@gmail.com>
2022-02-10 12:10:39 +0200

    Length

---
+++ Advanced/10.FunctionsExercise/7.LenghtChecker/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _7.LenghtChecker
+{
+    class Program
+    {
+        public delegate void LenghtCheck(string[] na,int n);
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            LenghtCheck lenght = (name, n) =>
+            {
+                Array.ForEach(name, x =>
+                 {
+                     if (x.Length <= n)
+                     {
+                         Console.WriteLine(x);
+                     }
+                 });
+            };
+            lenght(Console.ReadLine().Split(),n);
+        }
+    }
+}
