43c0d087d12fa306f3819cc6fd56f68606b4949c
hristianGH <hristovhristian7@gmail.com>
2022-02-10 12:09:48 +0200

    Sir

---
+++ Advanced/10.FunctionsExercise/2.AddSir/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _2.AddSir
+{
+    delegate void Each(string x);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Each each = x =>
+            {
+
+                foreach (var item in x.Split())
+                {
+                    Console.WriteLine($"Sir {item}" );
+                }
+            };
+            each(Console.ReadLine());
+        }
+    }
+}
