8b3936aa4e614e66071585b04dd7504c5289174c
hristianGH <hristovhristian7@gmail.com>
2022-02-11 10:22:38 +0200

    Car Plus

---
+++ Advanced/11.Classes/2.CarExtra/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _2.CarExtra
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+public class Car
+{
+    string make;
+    string model;
+    int year;
+    double fuelConsumption;
+    double fuelQuantity;
+    public string Make
+    {
+        set => make = value;
+    }
+    public string Model { set => model = value; }
+    public int Year
+    {
+        set => year = value;
+        get => year;
+    }
+    public Car(string[] arr)
+    {
+        int year = int.Parse(arr[2]);
+        string make = arr[1];
+        string model = arr[0];
+        make = this.make;
+        model = this.model;
+        year = this.year;
+    }
+}
