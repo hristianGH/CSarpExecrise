0ff2c64920efcae1b7862e619bb98bfe1fbecd17
hristianGH <hristovhristian7@gmail.com>
2022-02-11 10:22:32 +0200

    Car

---
+++ Advanced/11.Classes/0.Car/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _0.Car
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    public class Car
+    {
+        string make;
+        string model;
+        int year;
+        double fuelConsumption;
+        double fuelQuantity;
+        public string Make
+        {
+            set => make = value;
+        }
+        public string Model { set => model = value; }
+        public int Year
+        {
+            set => year = value;
+            get => year;
+        }
+    }
+}
