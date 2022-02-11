18f320351dbc9ecbd2e7cb875ea11369825df80a
hristianGH <hristovhristian7@gmail.com>
2022-02-11 10:23:03 +0200

    For Comit

---
+++ Advanced/11.Classes/4.CarEngine/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _4.CarEngine
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           //In Car Constructor 3.0
+        }
+    }
+}
