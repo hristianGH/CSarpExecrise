79d8f549da2aad715a1722daa3fc94a8d7839481
hristianGH <hristovhristian7@gmail.com>
2022-02-13 11:15:00 +0200

    Arr

---
+++ Advanced/13.Generics/ArrayCreator/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArrayCreator
+{
+   public class ArrayCreator
+    {
+        public static T[] Create<T>(int n,T input)
+        {
+            T[] arr = new T[n];
+            for (int i = 0; i < n-1; i++)
+            {
+                arr[i] = input;
+            }
+            return arr;
+        }
+    }
+}
---
+++ Advanced/13.Generics/ArrayCreator/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ArrayCreator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] str = ArrayCreator.Create(5, "Pesh");
+            int[] integ  = ArrayCreator.Create(10, 33);
+
+        }
+    }
+}
