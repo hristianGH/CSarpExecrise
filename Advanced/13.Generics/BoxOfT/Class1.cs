550530c58042eb4bda90ee80f3de8bb9fcde5fa8
hristianGH <hristovhristian7@gmail.com>
2022-02-13 11:15:21 +0200

    GenericBox

---
+++ Advanced/13.Generics/BoxOfT/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BoxOfT
+{
+    class Box<T>
+    {
+        public Box()
+        {
+            Stack<T> Item = new Stack<T>();
+            Items = Item;
+        }
+        public void Add(T element)
+        {
+            Items.Push(element);
+        }
+        public T Remove()
+        {
+            return Items.Pop();
+        }
+        public int Count { get; }
+        public Stack<T> Items { get; set; }
+    }
+}
