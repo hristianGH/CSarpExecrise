9139f797b899860f982a85d53dfa9a57edd80bda
hristianGH <hristovhristian7@gmail.com>
2022-02-13 11:15:33 +0200

    GenericScale

---
+++ Advanced/13.Generics/3.GenericScale/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _3.GenericScale
+{
+    public class EqualityScale <T1, T2>
+         where T1 : IComparable
+         where T2 : IComparable
+    {
+        public  bool Compare(T1 t1, T2 t2)
+        {
+            int res = t1.CompareTo(t2);
+            if (res>0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+    }
+}
---
+++ Advanced/13.Generics/3.GenericScale/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _3.GenericScale
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EqualityScale<int, int> equality = new EqualityScale<int, int>();
+            equality.Compare(15, 6);
+        }
+ 
+    }
+}
