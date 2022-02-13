68a36b9ed19e44b206c2a9ba821ed48eab7ccba6
hristianGH <hristovhristian7@gmail.com>
2022-02-13 11:14:52 +0200

    Box

---
+++ Advanced/13.Generics/1.Box/BoxofT.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BoxOfT
+{
+    public class BoxOfT<T>:IEnumerable<T>
+    {
+        public BoxOfT()
+        {
+            Count = 0;
+        }
+
+        public void Add(T value)
+        {
+            var curr = new Node<T>(value);
+            if (Last == null)
+            {
+                curr.Index = 0;
+                First = curr;
+                Last = curr;
+            }
+            else
+            {
+                curr.Index++;
+                Last.Next = curr;
+                Last = curr;
+            }
+        }
+        public void Remove()
+        {
+            Node<T> curr = First;
+            while (curr.Next!=null)
+            {
+
+            }
+        }
+        public IEnumerator<T> GetEnumerator()
+        {
+            Node<T> curr = First;
+            while (curr!=null)
+            {
+                yield return curr.Value;
+                curr = curr.Next;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+         
+
+        public int Count { get; set; }
+        public Node<T> Last { get; set; }
+        public Node<T> First { get; set; }
+    }
+}
+public class Node<T>
+{
+    public Node(T value)
+    {
+        Value = value;
+    }
+    public Node<T> Next { get; set; }
+    public T Value { get; set; }
+    public int Index { get;  set; }
+
+}
---
+++ Advanced/13.Generics/1.Box/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace BoxOfT
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var box = new BoxOfT<object>();
+            box.Add(6);
+            box.Add(7);
+            box.Add(6);
+            box.Add(7);
+            box.Add(6);
+            box.Add(7);
+            foreach (var item in box)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
