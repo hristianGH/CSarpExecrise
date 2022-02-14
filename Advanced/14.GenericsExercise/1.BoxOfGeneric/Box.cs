b6a6f0e9187bf4f253a410134472062fef12eaab
hristianGH <hristovhristian7@gmail.com>
2022-02-14 13:56:07 +0200

    BoxString

---
+++ Advanced/14.GenericsExercise/1.BoxOfGeneric/Box.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _1.BoxOfGeneric
+{
+    public class Box<T> : IEnumerable<T>
+    {
+        public Box(int range)
+        {
+            Value = new T[range];
+        }
+        public T[] Value { get; set; }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            foreach (var item in Value)
+            {
+                if (item!=null)
+                {
+                    yield return item;
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ Advanced/14.GenericsExercise/1.BoxOfGeneric/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _1.BoxOfGeneric
+{
+    public delegate void PrintAll(Box<int> numbers, Box<string> text);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintAll print = (x, y) =>
+            {
+                foreach (var item in x)
+                {
+                    Console.WriteLine(item);
+                }
+                foreach (var item in y)
+                {
+                    Console.WriteLine(item);
+                }
+            };
+
+            int n = int.Parse(Console.ReadLine());
+            string input = Console.ReadLine();
+            int value;
+            bool IsItINT = int.TryParse(input, out value);
+            if (IsItINT == true)
+            {
+
+                Box<int> boxInt = new Box<int>(n);
+                boxInt.Value[0] = int.Parse(input);
+                for (int i = 1; i < n; i++)
+                {
+                    boxInt.Value[i] = int.Parse(Console.ReadLine());
+                }
+                foreach (var item in boxInt)
+                {
+                    Console.WriteLine($"{item}");
+                }
+            }
+            else
+            {
+                Box<string> boxText = new Box<string>(n);
+                boxText.Value[0] = input;
+                for (int i = 1; i < n; i++)
+                {
+                    boxText.Value[i] = Console.ReadLine();
+                }
+                foreach (var item in boxText)
+                {
+                    Console.WriteLine($"{item}");
+                }
+            }
+             
+        }
+    }
+}
