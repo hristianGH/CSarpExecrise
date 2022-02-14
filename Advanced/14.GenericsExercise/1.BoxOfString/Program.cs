db883d8da489504fca435ff841a4d78954e1938b
hristianGH <hristovhristian7@gmail.com>
2022-02-14 13:56:24 +0200

    BoxString

---
+++ Advanced/14.GenericsExercise/1.BoxOfString/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _1.BoxOfString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            BoxString<int> box = new BoxString<int>(n);
+            for (int i = 0; i < n; i++)
+            {
+                box.Value[i] = int.Parse(Console.ReadLine());
+            }
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"{box} {box.Value[i]}");
+            }
+        }
+    }
+
+}
+
+class BoxString<T>
+{
+    public BoxString(int n)
+    {
+        Value = new T[n];
+    }
+    public T[] Value { get; set; }
+    public override string ToString()
+    {
+        return $"{Value.GetType()}:";
+    }
+}
+
