58c86f1e5f5ceb5e47b48734657d411664d4e997
hristianGH <hristovhristian7@gmail.com>
2022-02-16 10:21:35 +0200

    Stack

---
+++ Advanced/16.IteratorsAndComparators/3.CustomStack/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _3.CustomStack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inp = Console.ReadLine().Split(", ");
+           inp[0]= inp[0].Replace("Push ", "");
+            var stack =new MyStack(inp);
+            inp = Console.ReadLine().Split();
+            while (inp[0]!="END")
+            {
+                if (inp[0]=="Pop")
+                {
+                    stack.Pop();
+                }
+                inp = Console.ReadLine().Split();
+                if (inp[0]=="Push")
+                {
+                    stack.Push(inp[1]);
+                }
+            }
+            stack.Each();
+          //  stack.Each();
+
+        }
+    }
+}
---
+++ Advanced/16.IteratorsAndComparators/3.CustomStack/Stack.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3.CustomStack
+{
+    class MyStack
+    {
+        public MyStack(params string[] ar)
+        {
+            var node = new Node(ar[0]);
+            First = node;
+            node.Index = 0;
+            if (ar.Length > 0)
+            {
+                for (int i = 1; i < ar.Length; i++)
+                {
+                    var nodeNext = new Node(ar[i]);
+                    node.Next = nodeNext;
+                    node = node.Next;
+                    node.Index = i;
+                }
+                Count = node.Index + 1;
+            }
+        }
+        public Node First { get; set; }
+        public int Count { get; private set; }
+        public void Each()
+        {
+            if (First != null)
+            {
+
+                var curr = First;
+                Console.WriteLine(curr.Value);
+                while (curr.Next != null)
+                {
+                    curr = curr.Next;
+                    Console.WriteLine(curr.Value);
+                }
+            }
+            else
+            {
+            }
+        }
+        public void Pop()
+        {
+            var curr = First;
+            if (curr == null)
+            {
+                Console.WriteLine("No elements");
+            }
+            else if (curr.Next == null)
+            {
+                First = null;
+            }
+            else if (curr.Next.Next == null && curr.Next != null)
+            {
+                curr.Next = null;
+
+            }
+            else
+            {
+
+                while (curr.Next.Next != null)
+                {
+                    curr = curr.Next;
+                }
+                curr.Next = null;
+            }
+            Count--;
+        }
+        public void Push(string val)
+        {
+            var node = new Node(val);
+
+            if (First != null)
+            {
+                var curr = First;
+
+                while (curr.Next != null)
+                {
+                    curr = curr.Next;
+                }
+                curr.Next = node;
+                Count++;
+            }
+            else
+            {
+                First = node;
+                Count++;
+            }
+        }
+    }
+    class Node
+    {
+        public Node(string value)
+        {
+            Value = value;
+            Next = null;
+        }
+        public int Index { get; set; }
+        public Node Next { get; set; }
+        public string Value { get; set; }
+    }
+}
