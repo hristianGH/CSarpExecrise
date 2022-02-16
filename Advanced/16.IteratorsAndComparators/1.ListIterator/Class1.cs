c2e33adef8e035050be52b9b63699d8ef3c369d3
hristianGH <hristovhristian7@gmail.com>
2022-02-16 10:20:45 +0200

    ListIterator

---
+++ Advanced/16.IteratorsAndComparators/1.ListIterator/Class1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1.ListIterator
+{
+    class MyList:IEnumerable<string>
+    {
+        public MyList()
+        {
+            Item = new List<string>();
+        }
+        public MyList(params string[] arr)
+        {
+            Item = new List<string>(arr);
+            
+        }
+
+        public List<string> Item { get; set; }
+        public int Index { get; private set; }
+        public void Add(string ele)
+        {
+            Item.Add(ele);
+        }
+        public void Print()
+        {
+            Console.WriteLine(Item[Index]);
+        }
+        public bool Move()
+        {
+            if (Index +1 >Item.Count)
+            {
+                Console.WriteLine("Invalid Operation!");
+                return false;
+            }
+            else
+            {
+                Index++;
+                return true;
+            }
+        }
+        public bool HasNext()
+        {
+            if (Index+1>Item.Count-1)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public IEnumerator<string> GetEnumerator()
+        {
+            for (int i = 0; i < Item.Count; i++)
+            {
+                yield return Item[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+     
+}
---
+++ Advanced/16.IteratorsAndComparators/1.ListIterator/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace _1.ListIterator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new MyList();
+            bool isCreated = false;
+            string[] input = Console.ReadLine().Split();
+            while (input[0].ToUpper() != "END")
+            {
+                if (input[0] == "Create" && isCreated == false)
+                {
+                    if (input.Length > 0)
+                    {
+                        for (int i = 1; i < input.Length; i++)
+                        {
+                            list.Add(input[i]);
+                        }
+                        isCreated = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid Operation!");
+
+                    }
+                }
+               else if   (input[0]=="Print")
+                {
+                    if (list.Item.Count>0)
+                    {
+                        list.Print();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid Operation!");
+                    }
+                }
+               else if (input[0]=="Move")
+                {
+                    list.Move();
+                    Console.WriteLine(true);
+                }
+                else if (input[0]=="HasNext")
+                {
+                    Console.WriteLine(list.HasNext());
+                }
+                else if (input[0]=="PrintAll")
+                {
+                    Console.WriteLine(string.Join(" ",list));
+                }
+                input = Console.ReadLine().Split();
+            }
+        }
+        
+    }
+}
