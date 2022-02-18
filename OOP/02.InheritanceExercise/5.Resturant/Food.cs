87a1cbd161740ab922dc58467db11f03bc76a704
hristianGH <hristovhristian7@gmail.com>
2022-02-18 12:36:22 +0200

    Resturant

---
+++ OOP/02.InheritanceExercise/5.Resturant/Food.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5.Resturant
+{
+    class Food:Product
+    {
+        public Food(string name,double price,double grams):base(name,price)
+        {
+            Grams = grams;
+        }
+        public double Grams { get; set; }
+    }
+    class MiniDish:Food
+    {
+        public MiniDish(string name, double price, double grams):base(name, price, grams)
+        {
+
+        }
+    }
+    class Desert:Food
+    {
+        public Desert(string name, double price, double grams,double cal) :base(name,price,grams)
+        {
+            Calories = cal;
+        }
+         
+        public double Calories { get; set; }
+    }
+    class Starter:Food
+    {
+        public Starter( string name, double price, double grams ):base(name,price,grams)
+        {
+
+        }
+    }
+}
---
+++ OOP/02.InheritanceExercise/5.Resturant/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5.Resturant
+{
+    class Product
+    {
+        public Product(string n , double p)
+        {
+            Name = n;
+            Price = p;
+        }
+        public string Name { get; set; }
+        public double Price { get; set; }
+    }
+}
