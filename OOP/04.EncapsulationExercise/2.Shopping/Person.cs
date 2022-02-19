2530367b578c3f72c6912e86de815b45269274ca
hristianGH <hristovhristian7@gmail.com>
2022-02-19 14:59:10 +0200

    Shopping

---
+++ OOP/04.EncapsulationExercise/2.Shopping/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.Shopping
+{
+    class Person
+    {
+        public Person(string name,double money)
+        {
+            Name = name;
+            Money = money;
+            Products = new List<string>();
+        }
+        public double Money { get; set; }
+        public string Name { get; set; }
+        public List<string> Products { get; set; }
+    }
+}
---
+++ OOP/04.EncapsulationExercise/2.Shopping/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+namespace _2.Shopping
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(";");
+            Person[] people = new Person[input.Length];
+            for (int i = 0; i < input.Length; i++)
+            {
+                string[] split = input[i].Split("=", StringSplitOptions.RemoveEmptyEntries);
+                if (split[0] == "")
+                {
+                    Console.WriteLine("Name cannot be empty");
+                    Environment.Exit(0);
+
+                }
+                else if (double.Parse(split[1]) < 0)
+                {
+                    Console.WriteLine("Money cannot be negative");
+                    Environment.Exit(0);
+
+                }
+                else
+                {
+                    people[i] = new Person(split[0], double.Parse(split[1]));
+                }
+            }
+            input = Console.ReadLine().Split(";", StringSplitOptions.RemoveEmptyEntries);
+            Product[] products = new Product[input.Length];
+            for (int i = 0; i < input.Length; i++)
+            {
+                string[] split = input[i].Split("=");
+                if (split[0] == "")
+                {
+                    Console.WriteLine("Name cannot be empty");
+                    Environment.Exit(0);
+
+                }
+                else if (double.Parse(split[1]) < 0)
+                {
+                    Console.WriteLine("Money cannot be negative");
+                    Environment.Exit(0);
+                }
+                else
+                {
+                    products[i] = new Product(split[0], double.Parse(split[1]));
+                }
+            }
+            input = Console.ReadLine().Split();
+            while (input[0] != "END")
+            {
+                foreach (var person in people)
+                {
+                    if (input[0] == person.Name)
+                    {
+                        foreach (var product in products)
+                        {
+                            if (input[1] == product.Name)
+                            {
+                                if (person.Money - product.Price > -1)
+                                {
+                                    person.Money -= product.Price;
+                                    person.Products.Add(product.Name);
+                                    Console.WriteLine($"{person.Name} bought {product.Name}");
+                                }
+                                else
+                                {
+                                    Console.WriteLine($"{person.Name} can't affort {product.Name}");
+                                }
+                            }
+                        }
+                    }
+                }
+                input = Console.ReadLine().Split();
+            }
+            foreach (var item in people)
+            {
+                if (item.Products.Count < 1)
+                {
+                    Console.WriteLine($"{item.Name} - nothing bought");
+                }
+                else
+                {
+
+                    Console.WriteLine($"{item.Name} - {string.Join(", ", item.Products)}");
+                }
+            }
+        }
+    }
+}
