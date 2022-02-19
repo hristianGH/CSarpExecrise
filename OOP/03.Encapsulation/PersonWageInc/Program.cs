8698df35e94d56c7750e7b61ea3c94c27330dc40
hristianGH <hristovhristian7@gmail.com>
2022-02-19 11:26:12 +0200

    Wage

---
+++ OOP/03.Encapsulation/PersonWageInc/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace PersonWageInc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Person[] people = new Person[n];
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+                if (input.Length > 2)
+                {
+                    people[i] = new Person(input[0] + " " + input[1], int.Parse(input[2]), double.Parse(input[3]));
+                }
+
+            }
+            double incr = double.Parse(Console.ReadLine());
+            foreach (var item in people)
+            {
+                item.WageInc(incr);
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
