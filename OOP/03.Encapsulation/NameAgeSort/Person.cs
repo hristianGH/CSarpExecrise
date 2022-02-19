dc81f49d8076dc8a4a7ec882ddefda033a844a04
hristianGH <hristovhristian7@gmail.com>
2022-02-19 11:26:03 +0200

    NameSort

---
+++ OOP/03.Encapsulation/NameAgeSort/Person.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NameAgeSort
+{
+    class Person
+    {
+        public Person(string n,int a)
+        {
+            Name = n;
+            Age = a;
+        }
+        public Person(string n, int a,double w) :this(n,a)
+        {
+            Wage = w;
+        }
+        public String Name { get;private set; }
+        public int Age { get;private set; }
+        public double Wage { get;private set; }
+        public void WageInc(double perC)
+        {
+            perC = 0.01 * perC;
+            Wage = Wage * perC;
+        }
+        public override string ToString()
+        {
+            return $"{Name} is {Age} years old.";
+            //return $"{Name} receives {Wage:f2}";
+        }
+        //Asen Ivanov receives 2640.00 leva.
+    }
+}
---
+++ OOP/03.Encapsulation/NameAgeSort/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace NameAgeSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Person[] people = new Person[n];
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+                people[i] = new Person(input[0] + " " + input[1], int.Parse(input[2]));
+            }
+            
+            foreach (var item in people.OrderBy(x=>x.Name).ThenBy(x=>x.Name))
+            {
+                 
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
