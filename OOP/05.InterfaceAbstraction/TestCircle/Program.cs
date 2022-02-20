d4bf0cd013d0b536e84695809e791805bab6dd0c
hristianGH <hristovhristian7@gmail.com>
2022-02-20 11:19:07 +0200

    Circle

---
+++ OOP/05.InterfaceAbstraction/TestCircle/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace TestCircle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = 0;
+            double radius;
+            double thickness = 0.4;
+            char symbol = '*';
+            do
+            {
+                if (!double.TryParse(Console.ReadLine(), out radius) || radius <= 0)
+                {
+                    Console.WriteLine("radius have to be positive number");
+                }
+            }
+            while (radius <= 0);
+
+            Console.WriteLine();
+            double rIn = radius - thickness,
+                rOut = radius + thickness;
+
+            for (double y = radius; y >= -radius; --y)
+            {
+                for (double x = -radius; x < rOut; x += 0.4)
+                {
+                    time++;
+                    double value = x * x + y * y;
+                    if (value >= rIn * rIn && value <= rOut * rOut)
+                    {
+                         
+                         Console.Write(symbol);
+                    }
+                    else
+                    {
+                         Console.Write(" ");
+                    }
+                }
+                Console.WriteLine( );
+            }
+        }
+    }
+}
