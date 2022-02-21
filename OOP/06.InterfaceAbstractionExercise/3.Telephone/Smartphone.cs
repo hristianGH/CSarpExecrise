16efa6e7c63b1c7ada15951ca302421a50c98648
hristianGH <hristovhristian7@gmail.com>
2022-02-21 11:00:40 +0200

    Telephone

---
+++ OOP/06.InterfaceAbstractionExercise/3.Telephone/Smartphone.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3.Telephone
+{
+    class Smartphone : IPhone
+    {
+
+        public void Call(string number)
+        {
+            Console.WriteLine($"Calling... {number}");
+        }
+        public void Browse(string site)
+        {
+            Console.WriteLine($"Browsing: {site}!");
+        }
+        
+    }
+}
