6389fddf5544492de3058304f0cd29525ee156dc
hristianGH <hristovhristian7@gmail.com>
2022-02-21 11:01:05 +0200

    Border

---
+++ OOP/06.InterfaceAbstractionExercise/4.BorderControl/ID.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4.BorderControl
+{
+    class ID
+    {
+        public ID(string id)
+        {
+            Id = id;
+        }
+        public string Id { get; set; }
+    }
+}
