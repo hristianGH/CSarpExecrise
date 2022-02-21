79b8c2f485fb35d5b521168b050c4e0311a7238c
hristianGH <hristovhristian7@gmail.com>
2022-02-21 11:01:13 +0200

    Border

---
+++ OOP/06.InterfaceAbstractionExercise/4.BorderControl/Keys/IPerson.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4.BorderControl.Keys
+{
+    interface IPerson   
+    {
+        public int Age { get; set; }
+        public string Name { get; set; }
+        public bool IsRebel { get; set; }
+    }
+}
---
+++ OOP/06.InterfaceAbstractionExercise/4.BorderControl/Robot.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4.BorderControl
+{
+    class Robot :Keys.Iidentefiable
+    {
+        public Robot(string id, string name)
+        {
+            ID = id;
+            Model = name;
+        }
+        public string ID { get; set; }
+        public string Model { get; set; }
+    }
+}
