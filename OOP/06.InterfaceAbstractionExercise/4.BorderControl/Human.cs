b2401a8ff4bee5009259b2849e7b172e5c11805b
hristianGH <hristovhristian7@gmail.com>
2022-02-21 11:00:54 +0200

    Border

---
+++ OOP/06.InterfaceAbstractionExercise/4.BorderControl/Human.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4.BorderControl
+{
+    class Human : Keys.Iidentefiable, Keys.IBirthed, Keys.IPerson
+    {
+        public Human(string id, string name, int age, string bD)
+        {
+            ID = id;
+            Name = name;
+            Age = age;
+            DateTime date = DateTime.ParseExact(bD, "dd/MM/yyyy", System.Globalization.CultureInfo.InvariantCulture);
+            Birthdate = date;
+            IsRebel = false;
+            //dd/mm/yyyy
+        }
+        public string ID { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public DateTime Birthdate { get; set; }
+        public bool IsRebel { get ; set ; }
+    }
+    class Pet : Keys.IBirthed
+    {
+        public Pet(string n, string bD)
+        {
+            Name = n;
+            DateTime date = DateTime.ParseExact(bD, "dd/MM/yyyy", System.Globalization.CultureInfo.InvariantCulture);
+            Birthdate = date;
+        }
+        public string Name { get; set; }
+        public DateTime Birthdate { get; set; }
+    }
+    class Rebel : Keys.IPerson
+    {
+
+        public Rebel(string n, int a, string grp)
+        {
+            this.Name = n;
+            this.Age = a;
+            this.Group = grp;
+            IsRebel = true;
+        }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Group { get; set; }
+        public bool IsRebel { get; set; }
+
+    }
+}
+//public Citizen(string n, int a, string id, string bD)
+//{
+//    Name = n;
+//    Age = a;
+//    ID = id;
+//    DateTime date = DateTime.ParseExact(bD, "dd/MM/yyyy", System.Globalization.CultureInfo.InvariantCulture);
+//    Birthdate = date;
+//    //dd/mm/yyyy
+//}
+// public string Name { get; set; }
+// public int Age { get; set; }
+// public string ID { get; set; }
+// public DateTime Birthdate { get; set; }
+//    }
+//    class Pet : IBirthed
+// {
+//    public Pet(string n, string bD)
+//    {
+//        Name = n;
+//        DateTime date = DateTime.ParseExact(bD, "dd/MM/yyyy", System.Globalization.CultureInfo.InvariantCulture);
+//        Birthdate = date;
+//    }
+//    public string Name { get; set; }
+//    public DateTime Birthdate { get; set; }
---
+++ OOP/06.InterfaceAbstractionExercise/4.BorderControl/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _4.BorderControl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Food();
+
+        }
+        private static void Food()
+        {
+            int quantity = 0;
+            string[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            List<Keys.IPerson> list = new List<Keys.IPerson>();
+            while (input[0].ToUpper() != "END")
+            {
+                if (input.Length == 4)//Human Citizen
+                {
+                    list.Add(new Human(input[2], input[0], int.Parse(input[1]), input[3]));
+                }
+                else if (input.Length == 3)//Rebel
+                {
+                    list.Add(new Rebel(input[0], int.Parse(input[1]), input[2]));
+                }
+                else
+                {
+                    foreach (var item in list)
+                    {
+                        if (input[0] == item.Name)
+                        {
+                            if (item.IsRebel == false)
+                            {
+                                quantity += 10;
+                            }
+                            else
+                            {
+                                quantity += 5;
+                            }
+                        }
+                    }
+                }
+
+                input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            }
+            Console.WriteLine(quantity);
+        }
+        private static void Birthday()
+        {
+            //BirthDayExercise
+            List<Keys.IBirthed> list = new List<Keys.IBirthed>();
+            string[] input = Console.ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries);
+            while (input[0].ToUpper() != "END")
+            {
+                if (input[0].ToUpper() != "ROBOT")
+                {
+                    // Keys.IBirthed pet = new Pet(input[1] ,input[input.Length-1]);
+                    list.Add(new Pet(input[1], input[input.Length - 1]));
+                }
+                input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            }
+            input = Console.ReadLine().Split();
+            bool IsMoreThanZero = false;
+            foreach (var item in list)
+            {
+                int year = int.Parse(input[0]);
+                if (item.Birthdate.Year == year)
+                {
+                    Console.WriteLine(item.Birthdate.ToString("dd/MM//yyyy"));
+                    IsMoreThanZero = true;
+                    //24/12/2000
+                }
+            }
+            if (IsMoreThanZero == false)
+            {
+                Console.WriteLine("<empty output>");
+            }
+        }
+        private static void FindID()
+        {
+            //FindID exercise
+            List<Keys.Iidentefiable> ids = new List<Keys.Iidentefiable>();
+            string[] input = Console.ReadLine().Split();
+            while (input[0].ToUpper() != "END")
+            {
+                var id = input[input.Length - 1];
+                ids.Add(new Robot(id, input[0]));
+                input = Console.ReadLine().Split();
+            }
+            input[0] = Console.ReadLine();
+            foreach (var item in ids)
+            {
+                if (item.ID.EndsWith(input[0]))
+                {
+                    Console.WriteLine(item.ID);
+                }
+            }
+        }
+    }
+}
