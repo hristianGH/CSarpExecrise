407699cf5969450a9540914f91f7ed7edeadc514
hristianGH <hristovhristian7@gmail.com>
2022-02-22 10:53:17 +0200

    Shapes

---
+++ OOP/07.Polymorphism/3.Shapes/Circle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Shapes
+{
+    public class Circle : Shape
+    {
+        public Circle(double radius)
+        {
+            this.radius = radius;
+        }
+        private double radius;
+        public override double CalculateArea()
+        {
+            return (Math.PI * radius) * 2;
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return 2 * Math.PI * radius;
+        }
+        public override string Draw()
+        {
+            return base.Draw()+"Circle";
+        }
+    }
+}
---
+++ OOP/07.Polymorphism/3.Shapes/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Shapes
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rec = new Rectangle(3, 4);
+            Circle circle = new Circle(3);
+            Console.WriteLine(rec.Draw());
+            Console.WriteLine(circle.Draw());
+        }
+    }
+}
