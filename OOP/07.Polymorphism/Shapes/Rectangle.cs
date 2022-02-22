0e0046c5554617276254aeb7b3ce34a08b3774f4
hristianGH <hristovhristian7@gmail.com>
2022-02-22 10:54:00 +0200

    Shapes+

---
+++ OOP/07.Polymorphism/Shapes/Rectangle.cs
@@ -0,0 +1,28 @@
+﻿namespace Shapes
+{
+    public class Rectangle : Shape
+    {
+        private double height;
+        private double width;
+
+        public Rectangle(double hei, double wid)
+        {
+            height = hei;
+            width = wid;
+        }
+
+        public override double CalculateArea()
+        {
+            return this.height * this.width;
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return 2 * (height + width);
+        }
+        public override string Draw()
+        {
+            return base.Draw()+"Rectangle";
+        }
+    }
+}
