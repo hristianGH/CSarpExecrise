1aeff13642e28fe87e833700ee6a19577c012754
hristianGH <hristovhristian7@gmail.com>
2022-02-26 11:09:35 +0200

    Exercise

---
+++ OOP/14.UnitTestExercise/ConsoleApp/DatabaseTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace ConsoleApp
+{
+    public class DatabaseTests
+    {
+        Database database; 
+        [SetUp]
+        public void Setup()
+        {
+             database = new Database(new int[] { 1, 2, 3, 4 });
+        }
+
+        [Test]
+        public void AddOperation()
+        {
+            database.Add(5);
+            Assert.That(database.Count, Is.EqualTo(5));
+        }
+    }
+}
