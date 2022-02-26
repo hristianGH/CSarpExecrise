8b049f7d9af5afd92f0ce987749f61af9e0b7af9
hristianGH <hristovhristian7@gmail.com>
2022-02-26 11:09:27 +0200

    Lab

---
+++ OOP/13.UnitTest/Axe/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Axe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
