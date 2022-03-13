58ed29f0919d0a0d578d17ae432f725fe588469c
hristianGH <hristovhristian7@gmail.com>
2022-03-13 12:58:43 +0200

    Models

---
+++ DB Efcore/ExternalFormatProcessingJson/Shop/Data/Models/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Shop.Data.Models
+{
+    class Product
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [MinLength(3)]
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int SellerId { get; set; }
+        public User Seller { get; set; }
+        public int? BuyerId { get; set; }
+        public User Buyer { get; set; }
+        public ICollection<CategoryProduct> CategoryProducts { get; set; }
+        
+
+    }
+}
---
+++ DB Efcore/ExternalFormatProcessingJson/Shop/Data/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Shop.Data.Models
+{
+    class User
+    {
+
+        [Key]
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+        [MinLength(3)]
+        public string LastName { get; set; }
+        public int? Age { get; set; }
+        public ICollection<Product> ProductsSold { get; set; }
+        public ICollection<Product> ProductsBought { get; set; }
+
+    }
+}
