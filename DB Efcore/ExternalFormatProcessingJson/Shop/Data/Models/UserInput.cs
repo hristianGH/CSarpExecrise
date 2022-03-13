dcbc1f7b3442a6fe1c8c640396c619e4458843cf
hristianGH <hristovhristian7@gmail.com>
2022-03-13 12:59:01 +0200

    Models

---
+++ DB Efcore/ExternalFormatProcessingJson/Shop/Data/Models/UserInput.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shop.Data.Models
+{
+    class UserInput
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int? Age { get; set; }
+    }
+}
