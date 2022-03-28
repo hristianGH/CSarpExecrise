c05b4329d1a1431d1f6f90d442c9c11bd2037c2b
hristianGH <hristovhristian7@gmail.com>
2022-03-29 00:08:51 +0300

    Models

---
+++ SiteX/SiteX/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿namespace SiteX.Models
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public bool IsSoldOut { get; set; }
+        public TypeProd TypeProduct { get; set; }
+    }
+    public enum TypeProd
+    {
+        Shoes,
+        Shirt,
+        Pants,
+        Tshirts,
+        Socks,
+    }
+}
