15412f5791fa65a934f4c6158c80de197191fa05
hristianGH <hristovhristian7@gmail.com>
2022-03-29 18:55:04 +0300

    Revert "Revert "Added Models+Migration""
    This reverts commit ca3575b013028baee6aabcc82e5794a0c6262996.

---
+++ SiteX/SiteX.Data/Models/Blog/Poster.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiteX.Data.Models.Blog
+{
+    public class Poster
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required,MaxLength(100),MinLength(3)]
+        public string Name { get; set; }
+
+        [Required,MaxLength(100),MinLength(0)]
+        public string Description { get; set; }
+        public ICollection<Post> Posts { get; set; }=new List<Post>();
+    }
+}
