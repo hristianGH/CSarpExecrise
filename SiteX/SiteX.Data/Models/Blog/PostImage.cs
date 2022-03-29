6d63f7a6d4bb15dc125cdaea4c578e0fcb9da254
hristianGH <hristovhristian7@gmail.com>
2022-03-29 17:50:44 +0300

    Added Models+Migration

---
+++ SiteX/SiteX.Data/Models/Blog/PostImage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiteX.Data.Models.Blog
+{
+    public class PostImage
+    {
+
+        [Key]
+        public Guid Id { get; set; }
+        [Required]
+        public string Path { get; set; }
+    }
+}
