a642ec416c71f28a49f1bf5fc9122489265c56bf
hristianGH <hristovhristian7@gmail.com>
2022-03-31 15:35:56 +0300

    Api test

---
+++ SiteX/Data/SiteX.Data.Models/Shop/Api/RequestModelCategory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace SiteX.Data.Models.Shop.Api
+{
+    public class RequestModelCategory
+    {
+        [Required, MaxLength(100), MinLength(3)]
+        public string Name { get; set; }
+    }
+}
---
+++ SiteX/Web/SiteX.Web/Controllers/Api/ApiHomeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SiteX.Data.Models.Blog;
+using SiteX.Data.Models.Shop;
+using System.Threading.Tasks;
+
+namespace SiteX.Web.Controllers.Api
+{
+    [Route("/api/[controller]")]
+    [ApiController]
+    public class ApiHomeController : Controller
+    {
+        [HttpGet]
+        public  IActionResult GetPost(Post post)
+        {
+            return this.Ok(post);
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> CreatePost([FromForm]Post post)
+        {
+             
+            return this.Ok();
+        }
+    }
+}
