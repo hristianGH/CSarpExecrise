3972e7e24e25dd29cd9345e3f3bff7ee8d33d55f
hristianGH <hristovhristian7@gmail.com>
2022-04-07 21:50:27 +0300

    Genre and Post ViewModels

---
+++ SiteX/Web/SiteX.Web.ViewModels/BlogViewModels/GenreViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace SiteX.Web.ViewModels.ShopViewModels
+{
+    public class GenreViewModel
+    {
+        [Required]
+        [MaxLength(100)]
+        [MinLength(3)]
+        public string Name { get; set; }
+    }
+}
---
+++ SiteX/Web/SiteX.Web.ViewModels/BlogViewModels/PostViewModel.cs
@@ -0,0 +1,37 @@
+﻿using SiteX.Data.Models;
+using SiteX.Data.Models.Blog;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace SiteX.Web.ViewModels.BlogViewModels
+{
+    public class PostViewModel
+    {
+
+        [Required]
+        [MaxLength(100)]
+        [MinLength(10)]
+        public string Title { get; set; }
+
+        [Required]
+        [MaxLength(10_000)]
+        [MinLength(10)]
+        public string Body { get; set; }
+
+        public ApplicationUser User { get; set; }
+
+       
+
+        [MaxLength(4)]
+        [Display(Name = "Picture Url")]
+        public virtual ICollection<string> PostImages { get; set; } = new List<string>();
+
+        [Display(Name = "Genres")]
+        [MaxLength(2)]
+        public ICollection<int> PostGenres { get; set; } = new List<int>();
+
+
+    }
+}
