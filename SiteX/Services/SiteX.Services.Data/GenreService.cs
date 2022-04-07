31743a313714edcd29816994e8c799261278efb5
hristianGH <hristovhristian7@gmail.com>
2022-04-07 21:49:37 +0300

    GenreService

---
+++ SiteX/Services/SiteX.Services.Data/GenreService.cs
@@ -0,0 +1,34 @@
+﻿using SiteX.Data.Common.Repositories;
+using SiteX.Data.Models.Blog;
+using SiteX.Services.Data.Interface;
+using SiteX.Web.ViewModels.ShopViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiteX.Services.Data
+{
+    public class GenreService : IGenreService
+    {
+        private readonly IRepository<Genre> genreRepo;
+
+        public GenreService(IRepository<Genre> genreRepo)
+        {
+            this.genreRepo = genreRepo;
+        }
+        public async Task CreateAsync(GenreViewModel viewModel)
+        {
+            var genre = new Genre() { Name = viewModel.Name };
+            await this.genreRepo.AddAsync(genre);
+            await this.genreRepo.SaveChangesAsync();
+        }
+
+        public IEnumerable<Genre> GetGenres()
+        {
+            var genres = this.genreRepo.AllAsNoTracking().ToList();
+            return genres;
+        }
+    }
+}
