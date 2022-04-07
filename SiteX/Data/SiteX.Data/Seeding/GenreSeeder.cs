03f274b71a0609b9ec5858062a7cab829a2ad88a
hristianGH <hristovhristian7@gmail.com>
2022-04-07 21:49:19 +0300

    GenreSeeder

---
+++ SiteX/Data/SiteX.Data/Seeding/GenreSeeder.cs
@@ -0,0 +1,32 @@
+﻿using SiteX.Data.Models.Blog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiteX.Data.Seeding
+{
+    public class GenreSeeder:ISeeder
+    {
+        public async Task SeedAsync(ApplicationDbContext dbContext, IServiceProvider serviceProvider)
+        {
+
+            if (dbContext.Genres.Any())
+            {
+                return;
+            }
+
+            dbContext.Add(new Genre { Name = "Life and Living" });
+            dbContext.Add(new Genre { Name = "Life and Dying" });
+            dbContext.Add(new Genre { Name = "Life and Health" });
+            dbContext.Add(new Genre { Name = "Tech and Living" });
+            dbContext.Add(new Genre { Name = "Tech and Dying" });
+            dbContext.Add(new Genre { Name = "Tech and Health" });
+            dbContext.Add(new Genre { Name = "Misc" });
+
+
+
+        }
+    }
+}
