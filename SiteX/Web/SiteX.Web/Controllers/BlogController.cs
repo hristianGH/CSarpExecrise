12e31e8f7d969dbdd3ab96abaf101f18e80c206f
hristianGH <hristovhristian7@gmail.com>
2022-04-07 21:50:34 +0300

    Update BlogController.cs

--- SiteX/Web/SiteX.Web/Controllers/BlogController.cs
+++ SiteX/Web/SiteX.Web/Controllers/BlogController.cs
@@ -1,12 +1,90 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using SiteX.Data.Models;
+using SiteX.Services.Data;
+using SiteX.Services.Data.Interface;
+using SiteX.Web.ViewModels.BlogViewModels;
+using SiteX.Web.ViewModels.ShopViewModels;
+using System.Threading.Tasks;
 
 namespace SiteX.Web.Controllers
 {
     public class BlogController : Controller
     {
+        private readonly IGenreService genreService;
+        private readonly IPostService postService;
+        private readonly UserManager<ApplicationUser> userManager;
+
+        public BlogController(IGenreService genreService,
+            IPostService postService,
+            UserManager<ApplicationUser> userManager
+             )
+        {
+            this.genreService = genreService;
+            this.postService = postService;
+            this.userManager = userManager;
+        }
         public IActionResult Index()
         {
             return View();
+        }
+
+
+
+        [Authorize]
+        public IActionResult CreateGenre()
+        {
+            return this.View();
+        }
+
+        [HttpPost]
+        [Authorize]
+        public IActionResult CreateGenre(GenreViewModel viewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return this.BadRequest();
+            }
+            this.genreService.CreateAsync(viewModel);
+            return this.Redirect("/");
+        }
+
+        [Authorize]
+        public async Task<IActionResult> CreatePost()
+        {
+            this.ViewBag.Genres = new SelectList(this.genreService.GetGenres(), "Id", "Name");
+            return this.View();
+        }
+
+        [HttpPost]
+        [Authorize]
+        public async Task<IActionResult> CreatePost(PostViewModel viewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return this.BadRequest();
+            }
+            this.postService.CreatePostAsync(viewModel);
+            viewModel.User = await this.userManager.GetUserAsync(this.User);
+
+            return this.Redirect("/");
+        }
+
+
+
+        public async Task<IActionResult> CreateProduct(ProductViewModel viewModel)
+        {
+
+            if (!this.ModelState.IsValid)
+            {
+                return this.BadRequest();
+            }
+
+
+            await this.productService.CreateAsync(viewModel);
+            return this.Redirect("/");
         }
     }
 }
