abd3dc165ed4563f99df42aba58efe3281bf8af1
hristianGH <hristovhristian7@gmail.com>
2022-04-08 15:00:49 +0300

    Color Gender Size Service

---
+++ SiteX/Services/SiteX.Services.Data/ShopService/ColorService.cs
@@ -0,0 +1,30 @@
+﻿using SiteX.Data.Common.Repositories;
+using SiteX.Data.Models.Shop;
+using SiteX.Services.Data.ShopService.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SiteX.Services.Data.ShopService
+{
+    public class ColorService : IColorService
+    {
+        private readonly IRepository<Color> colorRepo;
+
+        public ColorService(IRepository<Color> colorRepo)
+        {
+            this.colorRepo = colorRepo;
+        }
+        public Dictionary<string, string> GetColorAsKVP()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Color> GetColors()
+        {
+            var colors = this.colorRepo.AllAsNoTracking().ToList();
+            return colors ;
+        }
+    }
+}
---
+++ SiteX/Services/SiteX.Services.Data/ShopService/SizeService.cs
@@ -0,0 +1,31 @@
+﻿using SiteX.Data.Common.Repositories;
+using SiteX.Data.Models.Shop;
+using SiteX.Services.Data.ShopService.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SiteX.Services.Data.ShopService
+{
+    public class SizeService : ISizeService
+    {
+        private readonly IRepository<Size> sizeRepo;
+
+        public SizeService(IRepository<Size> sizeRepo)
+        {
+            this.sizeRepo = sizeRepo;
+        }
+        public Dictionary<string, string> GetSizeAsKVP()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Size> GetSizes()
+        {
+            var sizes = this.sizeRepo.AllAsNoTracking().ToList();
+            return sizes;
+        }
+
+    }
+}
