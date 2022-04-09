c3d9fb05de871531befb85e80401d00b78c41085
hristianGH <hristovhristian7@gmail.com>
2022-04-09 20:24:08 +0300

    Size view model

---
+++ SiteX/Web/SiteX.Web.ViewModels/ShopViewModels/SizeViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SiteX.Web.ViewModels.ShopViewModels
+{
+    public class SizeViewModel
+    {
+        public string Name { get; set; }
+    }
+}
