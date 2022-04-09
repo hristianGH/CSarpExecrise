6dd20d3e12b034666df031959839ab57483eace5
hristianGH <hristovhristian7@gmail.com>
2022-04-09 20:23:41 +0300

    View model for BuyingProduct

---
+++ SiteX/Web/SiteX.Web.ViewModels/ShopViewModels/BuyingProductViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SiteX.Web.ViewModels.ShopViewModels
+{
+    public class BuyingProductViewModel
+    {
+        public ProductOutputViewModel Product { get; set; }
+        public string Color { get; set; }
+        public string Size { get; set; }
+
+    }
+}
