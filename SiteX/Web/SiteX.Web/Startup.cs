482a5984f874288117fd35550a4cfb3d68d4aca4
hristianGH <hristovhristian7@gmail.com>
2022-04-12 13:53:06 +0300

    Registered PostimageService

--- SiteX/Web/SiteX.Web/Startup.cs
+++ SiteX/Web/SiteX.Web/Startup.cs
@@ -88,6 +88,7 @@
             services.AddTransient<IGenreService, GenreService>();
             services.AddTransient<IPostGenreService, PostGenreSevice>();
             services.AddTransient<IPostService, PostService>();
+            services.AddTransient<IPostImageService, PostImageService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
