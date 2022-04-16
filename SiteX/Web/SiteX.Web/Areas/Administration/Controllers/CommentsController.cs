c2452e6ebc341baaa8cd3849fdf0e02af19cfe40
hristianGH <hristovhristian7@gmail.com>
2022-04-16 11:21:00 +0300

    Added Comment To Admin

---
+++ SiteX/Web/SiteX.Web/Areas/Administration/Controllers/CommentsController.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SiteX.Services.Data.BlogService.Interface;
+
+namespace SiteX.Web.Areas.Administration.Controllers
+{
+    public class CommentsController : AdministrationController
+    {
+        private readonly ICommentService commentService;
+
+        public CommentsController(ICommentService commentService)
+        {
+            this.commentService = commentService;
+        }
+        // GET: CommentsController
+        public IActionResult Index()
+        {
+            var comments = this.commentService.GetComents();
+            return this.View(comments);
+        }
+
+        // GET: CommentsController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: CommentsController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CommentsController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CommentsController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: CommentsController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CommentsController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: CommentsController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
