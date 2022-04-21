63c496ae3beb7dfe126b5b75a0352f1669d9969b
hristianGH <hristovhristian7@gmail.com>
2022-04-21 19:29:53 +0300

    Controller update

---
+++ SiteX/Web/SiteX.Web.ViewModels/ArticleViewModels/AllArticleViewModel.cs
@@ -0,0 +1,13 @@
+﻿using SiteX.Data.Models.Article;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SiteX.Web.ViewModels.ArticleViewModels
+{
+    public class AllArticleViewModel : PagingViewModel
+    {
+
+        public ICollection<Article> Articles { get; set; } = new List<Article>();
+    }
+}
---
+++ SiteX/Web/SiteX.Web/Controllers/ArticleController.cs
@@ -0,0 +1,47 @@
+﻿namespace SiteX.Web.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using SiteX.Services.Data.ArticleService.Interface;
+    using SiteX.Web.ViewModels.ArticleViewModels;
+    using SiteX.Web.ViewModels.BlogViewModels;
+
+    public class ArticleController : Controller
+    {
+        private readonly IArticleService articleService;
+
+        public ArticleController(IArticleService articleService)
+        {
+            this.articleService = articleService;
+        }
+
+        public IActionResult Index()
+        {
+            var articles = this.articleService.GetArticles();
+            if (articles != null)
+            {
+
+                return this.View(articles);
+            }
+            else
+            {
+                return this.NotFound();
+            }
+        }
+
+        public IActionResult All(int page = 1)
+        {
+
+            AllArticleViewModel articleViewModel = new AllArticleViewModel() { Articles = this.articleService.ToPage(page, 20), PageNumber = page, ItemsPerPage = 20 };
+
+            articleViewModel.ItemsCount = this.articleService.GetArticlesCount();
+
+            return this.View(articleViewModel);
+        }
+        public IActionResult ById(int id)
+        {
+            var article = this.articleService.GetArticleById(id);
+            return this.View(article);
+        }
+
+    }
+}
