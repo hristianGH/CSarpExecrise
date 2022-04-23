c866d505707fee89329a83c78475058a4c376118
hristianGH <hristovhristian7@gmail.com>
2022-04-23 17:29:12 +0300

    ProductTest

--- SiteX/Tests/SiteX.Services.Data.Tests/Shop/ProductTests/CreateProduct.cs
+++ SiteX/Tests/SiteX.Services.Data.Tests/Shop/ProductTests/CreateProduct.cs
@@ -26,8 +26,33 @@ namespace SiteX.Services.Data.Tests.Shop.ProductTests
             mockProductRepo.Setup(x => x.AddAsync(It.IsAny<Product>())).Callback((Product x) => list.Add(x));
             var service = new ProductService(mockProductRepo.Object);
 
+            var product = new ProductViewModel()
+            {
+                Name = "Big Shirt",
+                Price = 12,
+                Gender = "Unisex",
+                Categories = new int[] { 1, 2, 3 },
+                Locations = new int[] { 1 },
+                Colors = new int[] { 1, 2 },
+                Sizes = new int[] { 1, 2 },
+                Pictures = new string[] { "image1", "image2" },
+                Quantity = 22,
+                Id = new Guid(),
+                Description = "Product",
+
+            };
+            await service.CreateAsync(product);
 
             Assert.True(list.Count() > 0);
+            Assert.True(list[0].Name == "Big Shirt");
+            Assert.True(list[0].Price == 12);
+            Assert.True(list[0].Gender == "Unisex");
+            Assert.True(list[0].ProductCategories.Count()==3);
+            Assert.True(list[0].ProductLocations.Count()==1);
+            Assert.True(list[0].ProductColors.Count() == 2);
+            Assert.True(list[0].ProductSizes.Count() == 2);
+            Assert.True(list[0].Quantity == 22);
+
         }
 
     }
